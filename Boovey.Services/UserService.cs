b395d531e9995f91a3d595f155021a87912ac62d
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 02:30:09 +0200

    Initial commit

---
+++ Boovey.Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿namespace Boovey.Services
+{
+    using Boovey.Data.Entities;
+    using Boovey.Services.Interfaces;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    public class UserService : IUserService
+    {
+        public Task<User> GetCurrentUserAsync(ClaimsPrincipal principal)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
