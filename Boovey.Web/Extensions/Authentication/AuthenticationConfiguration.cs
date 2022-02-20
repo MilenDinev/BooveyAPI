28f8f582224461d42dca8d430e862e8d13301804
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 17:02:43 +0200

    Creating authenticatiocation functionality using jwt bearer authentication scheme

---
+++ Boovey.Web/Extensions/Authentication/AuthenticationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace Boovey.Web.Extensions.Authentication
+{
+    using Boovey.Web.Constants;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class AuthenticationConfiguration
+    {
+        public static void AddAuthentication(this IServiceCollection services)
+        {
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = IdentityServerConfigValues.URLAddress;
+                    options.Audience = IdentityServerConfigValues.ResourcesURLAddress;
+                });
+        }
+    }
+}
