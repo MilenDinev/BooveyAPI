11d0e87f9ad29a34e41da1a6ae5c092142a4df32
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 02:54:20 +0200

    Controller which handles get token requests made by users.

---
+++ Boovey.Web/Controllers/TokenController.cs
@@ -0,0 +1,28 @@
+﻿namespace Boovey.Web.Controllers
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Models.Requests;
+    using Models.Responses;
+    using Services.Interfaces;
+    using Extensions.Authentication;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : BooveyBaseController
+    {
+        public TokenController(IUserService userService) : base(userService)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TokenResponseModel>> Login(LoginRequestModel user)
+        {
+            using var httpClientHandler = new HttpClientHandler();
+            using var client = new HttpClient(httpClientHandler);
+            var token = await _userService.GetEligibilityTokenAsync(client, user.UsernameOrEmail, user.Password);
+            return token;
+        }
+    }
+}
