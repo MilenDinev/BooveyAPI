fe97e2c209e7c88b86509ce298e16c17ec1e986a
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 21:41:27 +0200

    Adding additional constants

--- Boovey.Web/Constants/SwaggerConfigValues.cs
+++ Boovey.Web/Constants/SwaggerConfigValues.cs
@@ -2,10 +2,19 @@
 {
     public static class SwaggerConfigValues
     {
+
+        public const string SwaggerDocVersion = "v1";
+        public const string SecurityDefinitionName = "Bearer";
+        public const string EndpointUrl = "/swagger/v1/swagger.json";
+        public const string EndpointName = "Boovey.Web v1";
         public const string OpenApiInfoTitle = "Boovey.Web";
         public const string OpenApiInfoVersion = "v1";
         public const string OpenApiInfoDescription = "Boovey API";
-        public const string EndpointUrl = "/swagger/v1/swagger.json";
-        public const string EndpointName = "Boovey.Web v1";
+        public const string OpenApiSecuritySchemeDescription = @"JWT Authorization header using the Bearer scheme.
+                                                               Example: \Authorization: Bearer {token}\";
+        public const string OpenApiSecuritySchemeName = "Authorization";
+        public const string OpenApiId = "Bearer";
+        public const string OpenApiScheme = "oauth2";
+        public const string OpenApiName = "Bearer";
     }
 }
