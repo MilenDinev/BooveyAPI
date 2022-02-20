8142654186971a56cdc1eb49f1ff09112b5ef054
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 21:14:14 +0200

    changing constants names

--- Boovey.Web/Constants/IdentityServerConfigValues.cs
+++ Boovey.Web/Constants/IdentityServerConfigValues.cs
@@ -2,9 +2,9 @@
 {
     public static class IdentityServerConfigValues
     {
+        public const string UrlAddress = @"https://localhost:5001";
+        public const string ResourcesUrlAddress = @"https://localhost:5001/resources";
         public const string TokenBaseAddress = @"https://localhost:5001/connect/token";
-        public const string URLAddress = @"https://localhost:5002";
-        public const string ResourcesURLAddress = @"https://localhost:5002/resources";
         public const string ClientId = "boovey";
         public const string SecretValue = "secret";
         public const string GrantType = "password";
