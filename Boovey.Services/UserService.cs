398a7dcb147580dc0516833d0192175da264c745
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 21:20:29 +0200

    Creating UserService and implmenting GetAllUsers and GetCurrentUser methods

--- Boovey.Services/UserService.cs
+++ Boovey.Services/UserService.cs
@@ -1,15 +1,38 @@
 ﻿namespace Boovey.Services
 {
-    using Boovey.Data.Entities;
-    using Boovey.Services.Interfaces;
-    using System.Security.Claims;
+    using System.Linq;
     using System.Threading.Tasks;
+    using System.Security.Claims;
+    using System.Collections.Generic;
+    using AutoMapper;
+    using Interfaces;
+    using Data;
+    using Data.Entities;
+    using Models.Responses.UserModels;
 
     public class UserService : IUserService
     {
-        public Task<User> GetCurrentUserAsync(ClaimsPrincipal principal)
+        private readonly IUserManager userManager;
+        private readonly BooveyDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public UserService(IUserManager userManager, BooveyDbContext dbContext, IMapper mapper)
         {
-            throw new System.NotImplementedException();
+            this.userManager = userManager;
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<User> GetCurrentUserAsync(ClaimsPrincipal principal)
+        {
+            return await this.userManager.GetUserAsync(principal);
+        }
+
+        public async Task<ICollection<UserResponseModel>> GetAllUsersAsync()
+        {
+            var users = await this.userManager.GetAllAsync();
+            var usersResponceDto = this.mapper.Map<ICollection<UserResponseModel>>(users);
+            return usersResponceDto.ToList();
         }
     }
 }
