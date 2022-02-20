b1f52a706658944a9a564474aa7eb8ed5214a0c9
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 21:24:39 +0200

    Creating UserMappingProfile class which will handle all user entities/models related mappings

---
+++ Boovey.Services/AutoMapperProfiles/User/UserMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace Boovey.Services.AutoMapperProfiles.User
+{
+    using AutoMapper;
+    using Boovey.Data.Entities;
+    using Boovey.Models.Responses.UserModels;
+
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            this.CreateMap<User, UserResponseModel>()
+                .ForMember(m => m.Email, e => e.MapFrom(u => u.Email ?? "none"));
+        }
+    }
+}
