c830a92360539ec7c4b318cec7cd0d7665573904
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 02:28:54 +0200

    Getting user's input in order authentication request to be processed.

---
+++ Boovey.Models/Requests/LoginRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Boovey.Models.Requests
+{
+    public class LoginRequestModel
+    {
+        public string UsernameOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
