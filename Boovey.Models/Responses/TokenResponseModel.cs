06ef2711a5ae836726752ad8a6d03b334273effa
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 02:29:43 +0200

    Token response which its used for authorization.

---
+++ Boovey.Models/Responses/TokenResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Boovey.Models.Responses
+{
+    using System.Text.Json.Serialization;
+
+    public class TokenResponseModel
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
