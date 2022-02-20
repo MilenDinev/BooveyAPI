2719a2ee68f65dfbfc760aaf1045fd41ac755a94
Milen Dinev <dinev.milen@gmail.com>
2022-02-20 17:02:06 +0200

    Adding URLAddress and ResourcesURLAddress constants

--- Boovey.Web/Constants/IdentityServerConfigValues.cs
+++ Boovey.Web/Constants/IdentityServerConfigValues.cs
@@ -3,6 +3,8 @@
     public static class IdentityServerConfigValues
     {
         public const string TokenBaseAddress = @"https://localhost:5001/connect/token";
+        public const string URLAddress = @"https://localhost:5002";
+        public const string ResourcesURLAddress = @"https://localhost:5002/resources";
         public const string ClientId = "boovey";
         public const string SecretValue = "secret";
         public const string GrantType = "password";
