cf6d176315b8a447742d7f605b6dd8e9a6c53878
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 00:28:12 +0200

    Implementing Add method

--- Boovey.Services/BookService.cs
+++ Boovey.Services/BookService.cs
@@ -3,6 +3,15 @@
     using AutoMapper;
     using Interfaces;
     using Data;
+    using System.Threading.Tasks;
+    using Boovey.Models.Responses.BookModels;
+    using Boovey.Models.Requests;
+    using Microsoft.EntityFrameworkCore;
+    using Boovey.Services.Constants;
+    using System;
+    using Boovey.Data.Entities.Books;
+    using System.Collections.Generic;
+    using Boovey.Data.Entities;
 
     public class BookService : IBookService
     {
@@ -14,5 +23,23 @@
             this.mapper = mapper;
         }
 
+        public async Task<AddedBookModel> Add(AddBookModel bookModel)
+        {
+            var book = await this.dbContext.Books.FirstOrDefaultAsync(b => b.ISBN == bookModel.ISBN) ?? 
+                throw new ArgumentException(string.Format(ErrorMessages.EntityAlreadyExists, nameof(Book), bookModel.Title));
+
+            book = mapper.Map<Book>(bookModel);
+            var authors = mapper.Map<ICollection<Author>>(bookModel.Authors);
+            var genres = mapper.Map<ICollection<Genre>>(bookModel.Genres);
+
+            book.Authors = authors;
+            book.Genres = genres;
+
+            await this.dbContext.Books.AddAsync(book);
+            await this.dbContext.SaveChangesAsync();
+
+
+            return mapper.Map<AddedBookModel>(book);
+        }
     }
 }
