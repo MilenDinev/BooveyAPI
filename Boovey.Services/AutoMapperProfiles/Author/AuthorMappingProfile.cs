248005622df88ce0669d12d20359e3795969625a
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 00:29:38 +0200

    Creating author mapping profile

---
+++ Boovey.Services/AutoMapperProfiles/Author/AuthorMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace Boovey.Services.AutoMapperProfiles.Author
+{
+    using AutoMapper;
+    using Data.Entities.Books;
+    using Models.Requests;
+    using Models.Responses.AuthorModels;
+
+    public class AuthorMappingProfile : Profile
+    {
+        public AuthorMappingProfile()
+        {
+            this.CreateMap<AddAuthorModel, Author>();
+            this.CreateMap<Author, AddedAuthorModel>();
+        }
+    }
+}
