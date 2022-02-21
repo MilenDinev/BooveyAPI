acea703ddc1d4eac7248674e637fcd7cb4634a47
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 00:28:57 +0200

    Creating Genre mapping profile

---
+++ Boovey.Services/AutoMapperProfiles/Genre/GenreMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace Boovey.Services.AutoMapperProfiles.Genre
+{
+    using AutoMapper;
+    using Data.Entities;
+    using Models.Requests;
+
+    public class GenreMappingProfile : Profile
+    {
+        public GenreMappingProfile()
+        {
+            this.CreateMap<AddGenreModel, Genre>();
+        }
+    }
+}
