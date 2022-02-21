6fd4e1d1acaa89eb0934e00f80181059f5529b30
Milen Dinev <dinev.milen@gmail.com>
2022-02-21 21:15:38 +0200

    New Migration created where Country class does not inherit from Entity class anymore

---
+++ Boovey.Data/Migrations/20220221190740_CountryDoesNotInheritEntity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Boovey.Data.Migrations
+{
+    public partial class CountryDoesNotInheritEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedOn",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierId",
+                table: "Countries");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Countries",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatorId",
+                table: "Countries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedOn",
+                table: "Countries",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastModifierId",
+                table: "Countries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
