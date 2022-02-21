c04b2ab2d38371fb0c28f5cf4807c22ac02998b9
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 00:29:27 +0200

    Creating book mapping profile

---
+++ Boovey.Services/AutoMapperProfiles/Book/BookMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace Boovey.Services.AutoMapperProfiles.Book
+{
+    using AutoMapper;
+    using Data.Entities.Books;
+    using Models.Requests;
+    using Models.Responses.BookModels;
+
+    public class BookMappingProfile : Profile
+    {
+        public BookMappingProfile()
+        {
+            this.CreateMap<AddBookModel, Book>();
+            this.CreateMap<Book, AddedBookModel>();
+        }
+    }
+}
