05567f31e5ac048289d51953648dfa843894cbed
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 00:28:41 +0200

    Defining Add method

--- Boovey.Services/Interfaces/IBookService.cs
+++ Boovey.Services/Interfaces/IBookService.cs
@@ -1,6 +1,11 @@
 ﻿namespace Boovey.Services.Interfaces
 {
+    using System.Threading.Tasks;
+    using Models.Requests;
+    using Models.Responses.BookModels;
+
     public interface IBookService
     {
+        Task<AddedBookModel> Add(AddBookModel bookModel);
     }
 }
