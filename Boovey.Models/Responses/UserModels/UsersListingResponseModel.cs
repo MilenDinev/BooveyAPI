55f2d70d006cf5fec576a0cfd3f5cd7fbe21e2e6
Milen Dinev <dinev.milen@gmail.com>
2022-02-21 13:18:26 +0200

    Listing all registered users response model

---
+++ Boovey.Models/Responses/UserModels/UsersListingResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Boovey.Models.Responses.UserModels
+{
+    public class UsersListingResponseModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
