0a6d04f0f4d1368bb0f62237946e3c69b3db84d2
Milen Dinev <dinev.milen@gmail.com>
2022-02-21 20:49:16 +0200

    Initial creation

---
+++ Boovey.Services/BookService.cs
@@ -0,0 +1,18 @@
+﻿namespace Boovey.Services
+{
+    using AutoMapper;
+    using Interfaces;
+    using Data;
+
+    public class BookService : IBookService
+    {
+        private readonly BooveyDbContext dbContext;
+        private readonly IMapper mapper;
+        public BookService(BooveyDbContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+    }
+}
