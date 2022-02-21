a916d925a8c2ecf6a720360ac347bf5f2d6ae749
Milen Dinev <dinev.milen@gmail.com>
2022-02-21 13:18:09 +0200

    Registration input model

---
+++ Boovey.Models/Requests/RegistrationRequestModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Boovey.Models.Requests
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegistrationRequestModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required!")]
+        [EmailAddress(ErrorMessage = "Please provide valid email address!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        public string Password { get; set; }
+    }
+}
