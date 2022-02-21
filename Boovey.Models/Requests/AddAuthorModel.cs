9211de6e3fce39510a44c9e16b7b0c7e240afec0
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 00:30:42 +0200

    Creating add author model

---
+++ Boovey.Models/Requests/AddAuthorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Boovey.Models.Requests
+{
+    public class AddAuthorModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Summary { get; set; }
+        public string Books { get; set; }
+    }
+}
