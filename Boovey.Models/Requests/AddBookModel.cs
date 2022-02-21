fc952da4272e7bc0588717702cee164899d71e16
Milen Dinev <dinev.milen@gmail.com>
2022-02-21 22:44:16 +0200

    Model which will be used for adding books in the database

---
+++ Boovey.Models/Requests/AddBookModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Boovey.Models.Requests
+{
+    public class AddBookModel
+    {
+
+        public string CoverUrl { get; set; }
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        public string PublicationDate { get; set; }
+        public string ASIN { get; set; }
+        public string ISBN { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public string Publisher  { get; set; }
+        public  string Genres { get; set; }
+        public  string Authors { get; set; }
+    }
+}
