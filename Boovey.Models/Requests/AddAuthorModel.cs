012ccb49796b8e435a1e55e1271d53c95b442159
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 23:32:51 +0200

    Removing summary and books from the model

--- Boovey.Models/Requests/AddAuthorModel.cs
+++ Boovey.Models/Requests/AddAuthorModel.cs
@@ -4,7 +4,5 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Summary { get; set; }
-        public string Books { get; set; }
     }
 }
