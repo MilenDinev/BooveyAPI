0f008f892fcf8f39db285bc0ac118dba23e0a061
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 23:35:27 +0200

    Implementing AddAsync method

--- Boovey.Services/BookService.cs
+++ Boovey.Services/BookService.cs
@@ -23,23 +23,50 @@
             this.mapper = mapper;
         }
 
-        public async Task<AddedBookModel> Add(AddBookModel bookModel)
+        public async Task<AddedBookModel> AddAsync(AddBookModel bookModel)
         {
-            var book = await this.dbContext.Books.FirstOrDefaultAsync(b => b.ISBN == bookModel.ISBN) ?? 
+            var book = await this.dbContext.Books.FirstOrDefaultAsync(b => b.ISBN == bookModel.ISBN);
+            if (book != null)
                 throw new ArgumentException(string.Format(ErrorMessages.EntityAlreadyExists, nameof(Book), bookModel.Title));
 
             book = mapper.Map<Book>(bookModel);
-            var authors = mapper.Map<ICollection<Author>>(bookModel.Authors);
-            var genres = mapper.Map<ICollection<Genre>>(bookModel.Genres);
+
+            var country = await this.dbContext.Countries.FirstOrDefaultAsync(c => c.Name == bookModel.Country);
+            book.Country = country;
+
+            var authors = new List<Author>();
+            foreach (var authorModel in bookModel.Authors)
+            {
+                var author = await this.dbContext.Authors.FirstOrDefaultAsync(a => a.FirstName.ToLower() == authorModel.FirstName.ToLower()
+                && a.LastName.ToLower() == authorModel.LastName.ToLower()) ?? mapper.Map<Author>(authorModel);
+                authors.Add(author);
+            }
 
             book.Authors = authors;
+
+            var genres = new List<Genre>();
+            foreach (var genreModel in bookModel.Genres)
+            {
+                var genre = await this.dbContext.Genres.FirstOrDefaultAsync(g => g.Title.ToLower() == genreModel.Title.ToLower()) ?? mapper.Map<Genre>(genreModel);
+                genres.Add(genre);
+            }
+
             book.Genres = genres;
+
+            var publisher = await this.dbContext.Publishers.FirstOrDefaultAsync(p => p.Name.ToLower() == bookModel.Publisher.Name.ToLower()) ?? mapper.Map<Publisher>(bookModel.Publisher);
+            book.Publisher = publisher;
 
             await this.dbContext.Books.AddAsync(book);
             await this.dbContext.SaveChangesAsync();
 
-
             return mapper.Map<AddedBookModel>(book);
+        }
+
+        public async Task<ICollection<BooksListingModel>> GetAllBooksAsync()
+        {
+            var books = await this.dbContext.Books.ToListAsync();
+
+            return mapper.Map<ICollection<BooksListingModel>>(books);
         }
     }
 }
