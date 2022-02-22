518259a26161c744014c103f9a5e157b7ccda224
Milen Dinev <dinev.milen@gmail.com>
2022-02-22 23:32:19 +0200

    Country entity has navigation property to books

--- Boovey.Data/Entities/Country.cs
+++ Boovey.Data/Entities/Country.cs
@@ -1,8 +1,17 @@
 ﻿namespace Boovey.Data.Entities
 {
+    using System.Collections.Generic;
+    using Books;
+
     public class Country
     {
+        public Country()
+        {
+            this.Books = new HashSet<Book>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
