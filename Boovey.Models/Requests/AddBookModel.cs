f5644db229f30db9b010c339a296fca491757bb3
Milen Dinev <dinev.milen@gmail.com>
2022-02-24 21:38:28 +0200

    ISBN regex validation added

--- Boovey.Models/Requests/AddBookModel.cs
+++ Boovey.Models/Requests/AddBookModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace Boovey.Models.Requests
 {
+    using Boovey.Models.Constants;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public class AddBookModel
     {
@@ -9,17 +11,20 @@
             this.Genres = new HashSet<AddGenreModel>();
             this.Authors = new HashSet<AddAuthorModel>();
         }
+
         public string CoverUrl { get; set; }
         public string Title { get; set; }
         public int Pages { get; set; }
         public string PublicationDate { get; set; }
+
+
+        [Required(ErrorMessage = ErrorMessages.ISBNRequiredProperty)]
+        [RegularExpression(BookModelConstants.ISBNValidation,ErrorMessage = ErrorMessages.InvalidISBNProperty)]
         public string ISBN { get; set; }
         public string Country { get; set; }
         public string Description { get; set; }
         public AddPublisherModel Publisher  { get; set; }
         public ICollection<AddGenreModel> Genres { get; set; }
         public ICollection<AddAuthorModel> Authors { get; set; }
-
-        //ISBN/ ASIN Validation 
     }
 }
