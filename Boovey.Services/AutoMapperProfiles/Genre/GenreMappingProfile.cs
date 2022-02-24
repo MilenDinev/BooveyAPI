a8f2b009288adaf6a65796cedd6dee300ec04cb5
Milen Dinev <dinev.milen@gmail.com>
2022-02-24 22:49:05 +0200

    Updateting mapping profile

--- Boovey.Services/AutoMapperProfiles/Genre/GenreMappingProfile.cs
+++ Boovey.Services/AutoMapperProfiles/Genre/GenreMappingProfile.cs
@@ -3,12 +3,15 @@
     using AutoMapper;
     using Data.Entities;
     using Models.Requests;
+    using System;
 
     public class GenreMappingProfile : Profile
     {
         public GenreMappingProfile()
         {
-            this.CreateMap<AddGenreModel, Genre>();
+            this.CreateMap<AddGenreModel, Genre>()
+            .ForMember(e => e.CreatedOn, m => m.MapFrom(d => DateTime.Now))
+            .ForMember(e => e.LastModifiedOn, m => m.MapFrom(d => DateTime.Now));
         }
     }
 }
--- Boovey.Services/AutoMapperProfiles/PublisherMappingProfile.cs
+++ Boovey.Services/AutoMapperProfiles/PublisherMappingProfile.cs
@@ -3,12 +3,15 @@
     using AutoMapper;
     using Models.Requests;
     using Data.Entities.Books;
+    using System;
 
     public class PublisherMappingProfile : Profile
     {
         public PublisherMappingProfile()
         {
-            this.CreateMap<AddPublisherModel, Publisher>();
+            this.CreateMap<AddPublisherModel, Publisher>()
+            .ForMember(e => e.CreatedOn, m => m.MapFrom(d => DateTime.Now))
+            .ForMember(e => e.LastModifiedOn, m => m.MapFrom(d => DateTime.Now));
         }
     }
 }
