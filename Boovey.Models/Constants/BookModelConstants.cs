deb66fdf66dc5130bff397d7e68bcd7951d410cc
Milen Dinev <dinev.milen@gmail.com>
2022-02-24 22:50:32 +0200

    Adding ISBNValidation regex constant

--- Boovey.Models/Constants/BookModelConstants.cs
+++ Boovey.Models/Constants/BookModelConstants.cs
@@ -2,6 +2,6 @@
 {
     internal static class BookModelConstants
     {
-        internal const string ISBNValidation = "^([0-9]{3})-([0-9]{2})-([0-9]{5})-([0-9]{2})-([0-9]{1})$";
+        internal const string ISBNValidation = "^([0-9]{3}|[0-9]{1})(-[0-9]{2,3})(-[0-9]{5})(-[0-9]{2}|[0-9]{0})(-[0-9]{1})$";
     }
 }
