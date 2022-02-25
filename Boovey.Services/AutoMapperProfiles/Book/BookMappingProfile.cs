c6d1923769d996a28d0318c70dec95a523fa9bfc
Milen Dinev <dinev.milen@gmail.com>
2022-02-25 04:06:51 +0200

    Updating mapping profile

--- Boovey.Services/AutoMapperProfiles/Book/BookMappingProfile.cs
+++ Boovey.Services/AutoMapperProfiles/Book/BookMappingProfile.cs
@@ -24,10 +24,10 @@
             .ForMember(m => m.Authors, e => e.MapFrom(b => string.Join(", ", b.Authors.Select(a => a.FirstName + " " + a.LastName))))
             .ForMember(m => m.Genres, e => e.MapFrom(b => string.Join(", ", b.Genres.Select(g => g.Title))))
             .ForMember(m => m.Publisher, e => e.MapFrom(b => b.Publisher.Name))
-            .ForMember(m => m.PublicationDate, e => e.MapFrom(b => b.PublicationDate.ToString("d")));
+            .ForMember(m => m.PublicationDate, e => e.MapFrom(b => b.PublicationDate.ToString("dd-MM-yyyy")));
             this.CreateMap<Book, BooksListingModel>()
             .ForMember(m => m.Country, e => e.MapFrom(b => b.Country.Name))
-            .ForMember(m => m.PublicationDate, e => e.MapFrom(b => b.PublicationDate.ToString("d")))
+            .ForMember(m => m.PublicationDate, e => e.MapFrom(b => b.PublicationDate.ToString("dd-MM-yyyy")))
             .ForMember(m => m.Authors, e => e.MapFrom(b => string.Join(", ", b.Authors.Select(a => a.FirstName + " " + a.LastName))))
             .ForMember(m => m.Genres, e => e.MapFrom(b => string.Join(", ", b.Genres.Select(g => g.Title))))
             .ForMember(m => m.Publisher, e => e.MapFrom(p => p.Publisher.Name));
