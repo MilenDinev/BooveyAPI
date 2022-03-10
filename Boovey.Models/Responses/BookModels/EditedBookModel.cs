2fddba51d7895813cb57c778555a39aa0f059e5d
Milen Dinev <dinev.milen@gmail.com>
2022-03-10 22:32:54 +0200

    Initial creation

---
+++ Boovey.Models/Responses/BookModels/EditedBookModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Boovey.Models.Responses.BookModels
+{
+    public class EditedBookModel
+    {
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public string Genres { get; set; }
+        public int Pages { get; set; }
+        public string Publisher { get; set; }
+        public string PublicationDate { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+    }
+}
+
