ab458ddb3c739d3ed00844f43be7bd2babbe483a
Milen Dinev <dinev.milen@gmail.com>
2022-03-10 22:33:32 +0200

    Adding IsNotFavorite error message. Refactoring

--- Boovey.Services/Constants/ErrorMessages.cs
+++ Boovey.Services/Constants/ErrorMessages.cs
@@ -3,8 +3,9 @@
     public static class ErrorMessages
     {
         public const string InvalidCredentials = "Invalid credentials, please try again!";
-        public const string EntityDoesNotExist = @"{0} does not exist!";
+        public const string EntityDoesNotExist = @"{0} '{1}' does not exist!";
         public const string EntityAlreadyExists = @"{0} '{1}' already exists in our system!";
         public const string IsAlreadyFavorite = @"{0} '{1}' already exists in favorites!";
+        public const string IsNotFavorite = @"{0} '{1}' does not exists in favorites!";
     }
 }
