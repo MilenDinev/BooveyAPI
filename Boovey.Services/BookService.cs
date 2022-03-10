bf9ca9ffd873c15ad4ad72b573a083a2be580a02
Milen Dinev <dinev.milen@gmail.com>
2022-03-10 22:34:25 +0200

    Implementing EditAsync and RemoveFavoriteBook methods. Refactoring

--- Boovey.Services/BookService.cs
+++ Boovey.Services/BookService.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using System.Globalization;
     using System.Threading.Tasks;
     using System.Collections.Generic;
     using Microsoft.EntityFrameworkCore;
@@ -32,14 +33,15 @@
 
             book = mapper.Map<Book>(bookModel);
 
-            var country = await this.dbContext.Countries.FirstOrDefaultAsync(c => c.Name == bookModel.Country) 
-                ?? throw new ArgumentException(string.Format(ErrorMessages.EntityDoesNotExist, nameof(Country))); 
+            var country = await this.dbContext.Countries.FirstOrDefaultAsync(c => c.Name == bookModel.Country)
+                ?? throw new ArgumentException(string.Format(ErrorMessages.EntityDoesNotExist, nameof(Country), bookModel.Country));
             book.Country = country;
 
             var authors = new List<Author>();
             foreach (var authorModel in bookModel.Authors)
             {
-                var author = await this.dbContext.Authors.FirstOrDefaultAsync(a => a.Fullname.ToLower() == authorModel.Fullname.ToLower()) ?? mapper.Map<Author>(authorModel);
+                var author = await this.dbContext.Authors.FirstOrDefaultAsync(a => a.Fullname.ToLower() == authorModel.Fullname.ToLower()) 
+                    ?? mapper.Map<Author>(authorModel);
                 authors.Add(author);
             }
 
@@ -48,13 +50,15 @@
             var genres = new List<Genre>();
             foreach (var genreModel in bookModel.Genres)
             {
-                var genre = await this.dbContext.Genres.FirstOrDefaultAsync(g => g.Title.ToLower() == genreModel.Title.ToLower()) ?? mapper.Map<Genre>(genreModel);
+                var genre = await this.dbContext.Genres.FirstOrDefaultAsync(g => g.Title.ToLower() == genreModel.Title.ToLower()) 
+                    ?? mapper.Map<Genre>(genreModel);
                 genres.Add(genre);
             }
 
             book.Genres = genres;
 
-            var publisher = await this.dbContext.Publishers.FirstOrDefaultAsync(p => p.Name.ToLower() == bookModel.Publisher.Name.ToLower()) ?? mapper.Map<Publisher>(bookModel.Publisher);
+            var publisher = await this.dbContext.Publishers.FirstOrDefaultAsync(p => p.Name.ToLower() == bookModel.Publisher.Name.ToLower()) 
+                ?? mapper.Map<Publisher>(bookModel.Publisher);
             book.Publisher = publisher;
             book.CreatorId = currentUserId;
             book.LastModifierId = currentUserId;
@@ -63,6 +67,38 @@
             await this.dbContext.SaveChangesAsync();
 
             return mapper.Map<AddedBookModel>(book);
+        }
+
+        public async Task<EditedBookModel> EditAsync(int bookId, EditBookModel bookModel, int currentUserId)
+        {
+            var book = await this.dbContext.Books.FirstOrDefaultAsync(b => b.Id == bookId)
+               ?? throw new ArgumentException(string.Format(ErrorMessages.EntityDoesNotExist, nameof(Book), bookModel.Title));
+
+            var country = await this.dbContext.Countries.FirstOrDefaultAsync(c => c.Name == bookModel.Country)
+                ?? throw new ArgumentException(string.Format(ErrorMessages.EntityDoesNotExist, nameof(Country), bookModel.Country));
+            book.Country = country;
+
+            var publisher = await this.dbContext.Publishers.FirstOrDefaultAsync(p => p.Id == bookModel.PublisherId)
+                ?? throw new ArgumentException(string.Format(ErrorMessages.EntityDoesNotExist, nameof(Publisher), bookModel.PublisherId));
+
+            var isValidDate = DateTime.TryParseExact(bookModel.PublicationDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime publicationDate);
+
+            // TOO FIX DATETIME INPUT IN ADD BOOK METHOD
+            if (!isValidDate)
+            {
+                throw new ArgumentException("Please provide valid publication date!");
+            }
+
+            book.Title = bookModel.Title;
+            book.Description = bookModel.Description;
+            book.Pages = bookModel.Pages;
+            book.PublicationDate = publicationDate;
+            book.Publisher = publisher;
+            book.LastModifierId = currentUserId;
+
+            await this.dbContext.SaveChangesAsync();
+
+            return mapper.Map<EditedBookModel>(book);
         }
 
         public async Task<AddedFavoriteBookModel> AddFavoriteBook(int bookId, User currentUser)
@@ -89,6 +125,22 @@
 
             await dbContext.SaveChangesAsync();
             return mapper.Map<AddedFavoriteBookModel>(book);
+        }
+
+        public async Task<RemovedFavoriteBookModel> RemoveFavoriteBook(int bookId, User currentUser)
+        {
+            var book = await this.dbContext.Books.FirstOrDefaultAsync(b => b.Id == bookId)
+                ?? throw new ArgumentException(string.Format(ErrorMessages.EntityDoesNotExist, nameof(Book)));
+
+            var isAlreadyFavoriteBook = currentUser.FavoriteBooks.FirstOrDefault(b => b.Id == bookId);
+
+            if (isAlreadyFavoriteBook == null)
+                throw new ArgumentException(string.Format(ErrorMessages.IsNotFavorite, nameof(Book), book.Title));
+
+            currentUser.FavoriteBooks.Remove(book);
+
+            await dbContext.SaveChangesAsync();
+            return mapper.Map<RemovedFavoriteBookModel>(book);
         }
 
         public async Task<ICollection<BooksListingModel>> GetAllBooksAsync()
