806b9cb7df1f95220a566afe20155c924e73e5fb
Milen Dinev <dinev.milen@gmail.com>
2022-03-11 02:47:58 +0200

    Country and Genres properties added

--- Boovey.Data/Entities/Books/Author.cs
+++ Boovey.Data/Entities/Books/Author.cs
@@ -7,12 +7,17 @@
         public Author()
         {
             this.Books = new HashSet<Book>();
+            this.Genres = new HashSet<Genre>();
             this.FavoriteByUsers = new HashSet<User>();
         }
 
         public string Fullname { get; set; }
+
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
         public string Summary { get; set; }
         public virtual ICollection<Book> Books { get; set; }
+        public virtual ICollection<Genre> Genres { get; set; }
         public virtual ICollection<User> FavoriteByUsers { get; set; }
     }
 }
