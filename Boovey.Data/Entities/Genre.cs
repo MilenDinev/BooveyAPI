b434d9994c7f96f7e62b075f88e5ca9a53393fca
Milen Dinev <dinev.milen@gmail.com>
2022-03-11 02:48:14 +0200

    Authors properties added

--- Boovey.Data/Entities/Genre.cs
+++ Boovey.Data/Entities/Genre.cs
@@ -8,11 +8,13 @@
         public Genre()
         {
             this.Books = new HashSet<Book>();
+            this.Authors = new HashSet<Author>();
             this.FavoriteByUsers = new HashSet<User>();
         }
 
         public string Title { get; set; }
         public virtual ICollection<Book> Books { get; set; }
+        public virtual ICollection<Author> Authors { get; set; }
         public virtual ICollection<User> FavoriteByUsers { get; set; }
     }
 }
