a19e7fb635c1107bc5722eb7e6f6f4e06a8611c1
Milen Dinev <dinev.milen@gmail.com>
2022-03-11 02:49:35 +0200

    Adding country id and refactoring

--- Boovey.Models/Responses/AuthorModels/AddedAuthorModel.cs
+++ Boovey.Models/Responses/AuthorModels/AddedAuthorModel.cs
@@ -2,7 +2,8 @@
 {
     public class AddedAuthorModel
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Fullname { get; set; }
+        public string Summary { get; set; }
+        public int CountryId { get; set; }
     }
 }
