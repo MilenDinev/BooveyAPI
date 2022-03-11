61eef7fb021c9f934859f82da8158868b6b79461
Milen Dinev <dinev.milen@gmail.com>
2022-03-11 02:49:23 +0200

    Adding country id

--- Boovey.Models/Requests/AddAuthorModel.cs
+++ Boovey.Models/Requests/AddAuthorModel.cs
@@ -3,6 +3,7 @@
     public class AddAuthorModel
     {
         public string Fullname { get; set; }
+        public int CountryId { get; set; }
         public string Summary { get; set; }
     }
 }
