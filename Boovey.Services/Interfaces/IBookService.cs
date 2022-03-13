483bc2852bacd1efe4305e5579f989b5878eb2d7
Milen Dinev <dinev.milen@gmail.com>
2022-03-13 03:39:29 +0200

    Defining AssignAuthorAsync and AssignGenreAsync

--- Boovey.Services/Interfaces/IBookService.cs
+++ Boovey.Services/Interfaces/IBookService.cs
@@ -5,11 +5,14 @@
     using Data.Entities;
     using Models.Requests;
     using Models.Responses.BookModels;
+    using Models.Responses.SharedModels;
 
     public interface IBookService
     {
         Task<AddedBookModel> AddAsync(AddBookModel bookModel, int currentUserId);
         Task<EditedBookModel> EditAsync(int bookId, EditBookModel bookModel, int currentUserId);
+        Task<AssignedAuthorBookModel> AssignAuthorAsync(int bookId, int authorId, int modifierId);
+        Task<AssignedBookGenreModel> AssignGenreAsync(int bookId, int genreId, int modifierId);
         Task<AddedFavoriteBookModel> AddFavoriteBook(int bookId, User currentUser);
         Task<RemovedFavoriteBookModel> RemoveFavoriteBook(int bookId, User currentUser);
         Task<ICollection<BooksListingModel>> GetAllBooksAsync();
