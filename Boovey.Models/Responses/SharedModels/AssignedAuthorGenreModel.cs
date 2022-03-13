db4281851bd1b4049a3c04277c35cb5cf6eca7b8
Milen Dinev <dinev.milen@gmail.com>
2022-03-13 03:13:02 +0200

    Initial Creation

---
+++ Boovey.Models/Responses/SharedModels/AssignedAuthorGenreModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Boovey.Models.Responses.SharedModels
+{
+    public class AssignedAuthorGenreModel
+    {
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
---
+++ Boovey.Models/Responses/SharedModels/UnassignedAuthorBookModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Boovey.Models.Responses.SharedModels
+{
+    public class UnassignedAuthorBookModel
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
