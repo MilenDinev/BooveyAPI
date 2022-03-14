5669f8228b19e1dd3816797edaac990cc7f3312e
Milen Dinev <dinev.milen@gmail.com>
2022-03-14 05:03:28 +0200

    Defining AddAsync, EditAsync, AddFavoriteGenre, RemoveFavoriteGenre, GetAllGenresAsync methods

--- Boovey.Services/Interfaces/IGenreService.cs
+++ Boovey.Services/Interfaces/IGenreService.cs
@@ -1,6 +1,17 @@
 ﻿namespace Boovey.Services.Interfaces
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Data.Entities;
+    using Models.Requests.GenreModels;
+    using Models.Responses.GenreModels;
+
     public interface IGenreService
     {
+        Task<AddedGenreModel> AddAsync(AddGenreModel genreModel, int currentUserId);
+        Task<EditedGenreModel> EditAsync(int genreId, EditGenreModel genreModel, int currentUserId);
+        Task<AddedFavoriteGenreModel> AddFavoriteGenre(int genreId, User currentUser);
+        Task<RemovedFavoriteGenreModel> RemoveFavoriteGenre(int genreId, User currentUser);
+        Task<ICollection<GenreListingModel>> GetAllGenresAsync();
     }
 }
