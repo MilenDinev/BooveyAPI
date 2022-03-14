6d82292ae12567ae0233982246f347087b1e9609
Milen Dinev <dinev.milen@gmail.com>
2022-03-14 07:19:15 +0200

    Changing property name from Title to Name

--- Boovey.Models/Responses/PublisherModels/AddedPublisherModel.cs
+++ Boovey.Models/Responses/PublisherModels/AddedPublisherModel.cs
@@ -3,6 +3,6 @@
     public class AddedPublisherModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
     }
 }
--- Boovey.Models/Responses/PublisherModels/EditedPublisherModel.cs
+++ Boovey.Models/Responses/PublisherModels/EditedPublisherModel.cs
@@ -3,6 +3,6 @@
     public class EditedPublisherModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
     }
 }
