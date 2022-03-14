1280c085e0d61a1b67fed1a8bea8d5fa66e38af2
Milen Dinev <dinev.milen@gmail.com>
2022-03-14 07:02:38 +0200

    Initial Creation

---
+++ Boovey.Models/Responses/PublisherModels/AddedPublisherModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Boovey.Models.Responses.PublisherModels
+{
+    public class AddedPublisherModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
