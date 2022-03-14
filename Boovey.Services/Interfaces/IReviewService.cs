2932881e95227a94020ad255991469d69ad3eae9
Milen Dinev <dinev.milen@gmail.com>
2022-03-14 03:47:10 +0200

    Refactoring

--- Boovey.Services/Interfaces/IReviewService.cs
+++ Boovey.Services/Interfaces/IReviewService.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Boovey.Services.Interfaces
+﻿namespace Boovey.Services.Interfaces
 {
-    internal interface IReviewService
+    public interface IReviewService
     {
     }
 }
