5d6d536c811c192f3d57dac121a460e26e988664
Milen Dinev <dinev.milen@gmail.com>
2022-03-14 07:43:35 +0200

    Adding Author navigational property in Quote both AuthorId and BookId are nullable

--- Boovey.Data/Entities/Author.cs
+++ Boovey.Data/Entities/Author.cs
@@ -8,6 +8,7 @@
         {
             this.Books = new HashSet<Book>();
             this.Genres = new HashSet<Genre>();
+            this.Quotes = new HashSet<Quote>();
             this.FavoriteByUsers = new HashSet<User>();
         }
 
@@ -18,6 +19,7 @@
         public string Summary { get; set; }
         public virtual ICollection<Book> Books { get; set; }
         public virtual ICollection<Genre> Genres { get; set; }
+        public virtual ICollection<Quote> Quotes { get; set; }
         public virtual ICollection<User> FavoriteByUsers { get; set; }
     }
 }
--- Boovey.Data/Entities/Quote.cs
+++ Boovey.Data/Entities/Quote.cs
@@ -11,7 +11,9 @@
 
         public string Content { get; set; }
         public int Likes { get; set; }
-        public int BookId { get; set; }
+        public int? AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        public int? BookId { get; set; }
         public virtual Book Book { get; set; }
         public virtual ICollection<User> FavoriteByUsers { get; set; }
     }
