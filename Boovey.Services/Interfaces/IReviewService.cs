99917cd1dfb5fe8ade5bdfd8f74b5134e957b8ce
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 01:00:23 +0200

    Defining AddAsync  and EditAsync methods

--- Boovey.Services/Interfaces/IReviewService.cs
+++ Boovey.Services/Interfaces/IReviewService.cs
@@ -1,6 +1,12 @@
 ﻿namespace Boovey.Services.Interfaces
 {
+    using System.Threading.Tasks;
+    using Models.Requests.ReviewModels;
+    using Models.Responses.ReviewModels;
+
     public interface IReviewService
     {
+        Task<AddedReviewModel> AddAsync(AddReviewModel reviewModel, int currentUserId);
+        Task<EditedReviewModel> EditAsync(int reviewId, EditReviewModel reviewModel, int currentUserId);
     }
 }
