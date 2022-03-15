ddbb55c2ebf14aa8d24f8434506d0d72b2cccde0
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 00:57:44 +0200

    Review Configuration migration

---
+++ Boovey.Data/Migrations/20220315183720_CreatingReviewConfig.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Boovey.Data.Migrations
+{
+    public partial class CreatingReviewConfig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Reviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_CreatorId",
+                table: "Reviews",
+                column: "CreatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_CreatorId",
+                table: "Reviews",
+                column: "CreatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_CreatorId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_CreatorId",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Reviews",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
