df8ff658d69549f08a68d4c791b9e92d1b24ac2c
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 01:35:04 +0200

    Initial creation

---
+++ Boovey.Data/Configurations/Users/UserShelveConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Boovey.Data.Configurations.Users
+{
+    using System.Collections.Generic;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Entities;
+
+    public class UserShelveConfiguration : IEntityTypeConfiguration<Shelve>
+    {
+        public void Configure(EntityTypeBuilder<Shelve> builder)
+        {
+            builder.HasMany(g => g.FavoriteByUsers)
+             .WithMany(u => u.FavoriteShelves)
+                .UsingEntity<Dictionary<string, object>>("UserShelves",
+                x => x.HasOne<User>().WithMany().HasForeignKey("UserId")
+                      .OnDelete(DeleteBehavior.Restrict),
+                x => x.HasOne<Shelve>().WithMany().HasForeignKey("ShelveId")
+                      .OnDelete(DeleteBehavior.Restrict));
+        }
+    }
+}
