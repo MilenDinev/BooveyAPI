d2f2d35c868305985541cff84f6bf242649c9ba9
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 00:57:44 +0200

    Review Configuration migration

--- Boovey.Data/Migrations/20220315183720_CreatingReviewConfig.Designer.cs
+++ Boovey.Data/Migrations/20220315183720_CreatingReviewConfig.Designer.cs
@@ -4,14 +4,16 @@ using Boovey.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Boovey.Data.Migrations
 {
     [DbContext(typeof(BooveyDbContext))]
-    partial class BooveyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220315183720_CreatingReviewConfig")]
+    partial class CreatingReviewConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
