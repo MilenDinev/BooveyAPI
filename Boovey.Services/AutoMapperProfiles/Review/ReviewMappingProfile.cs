5bb6d7e56b4d998bbcb1443a04efe9278f12f780
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 00:59:47 +0200

    Review Mapping profile initial creation

---
+++ Boovey.Services/AutoMapperProfiles/Review/ReviewMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace Boovey.Services.AutoMapperProfiles.Review
+{
+    using System;
+    using AutoMapper;
+    using Data.Entities;
+    using Models.Requests.ReviewModels;
+    using Models.Responses.ReviewModels;
+
+    public class ReviewMappingProfile : Profile
+    {
+        public ReviewMappingProfile()
+        {
+            this.CreateMap<AddReviewModel, Review>()
+            .ForMember(e => e.CreatedOn, m => m.MapFrom(d => DateTime.Now))
+            .ForMember(e => e.LastModifiedOn, m => m.MapFrom(d => DateTime.Now));
+            this.CreateMap<Review, AddedReviewModel>();
+            this.CreateMap<Review, EditedReviewModel>();
+            this.CreateMap<Review, ReviewListingModel>();
+        }
+    }
+}
