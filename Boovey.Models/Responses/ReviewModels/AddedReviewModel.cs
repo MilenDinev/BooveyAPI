cfc58981fc4f3d031f0cac00c801fcfb7b9436e2
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 00:59:12 +0200

    Initial creation

---
+++ Boovey.Models/Responses/ReviewModels/AddedReviewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Boovey.Models.Responses.ReviewModels
+{
+    public class AddedReviewModel
+    {
+        public int Id { get; set; }
+        public double Rating { get; set; }
+        public string Comment { get; set; }
+        public int BookId { get; set; }
+    }
+}
