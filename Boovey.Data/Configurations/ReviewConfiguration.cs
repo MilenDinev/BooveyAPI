9ae60dc4a04ffe3e33e1ad875f9951cf46c223aa
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 00:58:08 +0200

    Review Configuration initial creation

---
+++ Boovey.Data/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Boovey.Data.Configurations.Users
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Entities;
+
+    public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.HasOne(r => r.Creator)
+            .WithMany(u => u.Reviews)
+            .HasForeignKey(r => r.CreatorId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
