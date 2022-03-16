db81e85cb33bc803b8d84a7973d52279f8c88d3b
Milen Dinev <dinev.milen@gmail.com>
2022-03-16 19:40:00 +0200

    Initial creation of CountryService

---
+++ Boovey.Services/CountryService.cs
@@ -0,0 +1,27 @@
+﻿namespace Boovey.Services
+{
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Interfaces;
+    using Data;
+    using Data.Entities;
+    using Services.Constants;
+    using Services.Exceptions;
+
+    public class CountryService : ICountryService
+    {
+        private readonly BooveyDbContext dbContext;
+
+        public CountryService(BooveyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task FindCountryById(int countryId)
+        {
+            var isCountryExists = await this.dbContext.Countries.AnyAsync(c => c.Id == countryId)
+                ? true
+                : throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(Country), countryId));
+        }
+    }
+}
---
+++ Boovey.Services/Interfaces/ICountryService.cs
@@ -0,0 +1,9 @@
+﻿namespace Boovey.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    public interface ICountryService
+    {
+        Task FindCountryById(int countryId);
+    }
+}
