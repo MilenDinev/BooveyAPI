f65b567eb927d89b51119a44d18103f99f75f66f
Milen Dinev <dinev.milen@gmail.com>
2022-04-10 02:07:53 +0300

    Creating EntityAlreadyContained and EntityHasBeenDeleted error messages

--- Boovey.Services/Constants/ErrorMessages.cs
+++ Boovey.Services/Constants/ErrorMessages.cs
@@ -8,7 +8,8 @@
         public const string EntityIdDoesNotExist = @"{0} with id '{1}' does not exist!";
         public const string EntityAlreadyExists = @"{0} '{1}' already exists in our system!";
         public const string EntityAlreadyAssignedId = @"{0} with id '{1}' has already been assigned to {2} with id '{3}'!";
-        public const string EntityAlreadyCreatedByUser = @"{0} has already been created by the user!";
+        public const string EntityAlreadyContained = @"{0} has already exists in current collection!";
+        public const string EntityHasBeenDeleted = @"{0} has already been deleted and cannot be modified!";
         public const string AlreadyFavoriteId = @"{0} with id '{1}' already exists in favorites!";
         public const string NotFavoriteId = @"{0} with id '{1}' does not exists in favorites!";
         public const string AlreadyFollowing = @"{0} '{1}' has already been followed by you!";
