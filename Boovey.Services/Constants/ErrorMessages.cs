ca6081207dede717f67b20eb2f5ec571104f8c5e
Milen Dinev <dinev.milen@gmail.com>
2022-04-09 23:55:40 +0300

    Adding EntityAlreadyCreatedByUser message

--- Boovey.Services/Constants/ErrorMessages.cs
+++ Boovey.Services/Constants/ErrorMessages.cs
@@ -7,10 +7,11 @@
         public const string EntityDoesNotExist = @"{0} '{1}' does not exist!";
         public const string EntityIdDoesNotExist = @"{0} with id '{1}' does not exist!";
         public const string EntityAlreadyExists = @"{0} '{1}' already exists in our system!";
-        public const string EntityAlreadyAssignedId = @"{0} with id '{1}' is already assigned to {2} with id '{3}'!";
+        public const string EntityAlreadyAssignedId = @"{0} with id '{1}' has already been assigned to {2} with id '{3}'!";
+        public const string EntityAlreadyCreatedByUser = @"{0} has already been created by user with id '{2}'!!";
         public const string AlreadyFavoriteId = @"{0} with id '{1}' already exists in favorites!";
         public const string NotFavoriteId = @"{0} with id '{1}' does not exists in favorites!";
-        public const string AlreadyFollowing = @"{0} '{1}' is already followed by you!";
+        public const string AlreadyFollowing = @"{0} '{1}' has already been followed by you!";
         public const string FollowingItSelf = @"Following yourself is forbidden!";
     }
 }
