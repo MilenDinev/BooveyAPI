11584d79d64b938d1335ac11bf6ebf2824cda648
Milen Dinev <dinev.milen@gmail.com>
2022-04-10 22:04:43 +0300

    Initial Creation

---
+++ Boovey.Models/Responses/BookModels/DeletedBookModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Boovey.Models.Responses.BookModels
+{
+    public class DeletedBookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+    }
+}
