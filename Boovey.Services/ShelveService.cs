ec4365b210698f5a0398f1f82e907c5a25841516
Milen Dinev <dinev.milen@gmail.com>
2022-04-13 22:50:16 +0300

    Inherits from BaseService class, Refactoring, renaming methods

--- Boovey.Services/ShelveService.cs
+++ Boovey.Services/ShelveService.cs
@@ -14,23 +14,20 @@
     using Models.Requests.ShelveModels;
     using Models.Responses.ShelveModels;
 
-    public class ShelveService : IShelveService
+    public class ShelveService : BaseService<Shelve>, IShelveService
     {
-        private readonly BooveyDbContext dbContext;
         private readonly IMapper mapper;
 
-        public ShelveService(BooveyDbContext dbContext, IMapper mapper)
+        public ShelveService(BooveyDbContext dbContext, IMapper mapper) : base(dbContext)
         {
-            this.dbContext = dbContext;
             this.mapper = mapper;
         }
+
         public async Task<Shelve> CreateAsync(CreateShelveModel model, int creatorId)
         {
             var shelve = this.mapper.Map<Shelve>(model);
 
-            await SetCreatorAsync(shelve, creatorId) ;
-            await this.dbContext.AddAsync(shelve);
-            await SaveModificationAsync(shelve, creatorId);
+            await AddEntityAsync(shelve, creatorId);
 
             return shelve;
         }
@@ -41,8 +38,7 @@
         }
         public async Task DeleteAsync(Shelve shelve, int modifierId)
         {
-            shelve.Deleted = true;
-            await SaveModificationAsync(shelve, modifierId);
+            await DeleteEntityAsync(shelve, modifierId);
         }
 
         public async Task<AddedFavoriteShelveModel> AddFavoriteAsync(int shelveId, User currentUser)
@@ -74,6 +70,20 @@
             return mapper.Map<RemovedFavoriteShelveModel>(shelve);
         }
 
+        public async Task<Shelve> GetByIdAsync(int shelveId)
+        {
+            var shelve = await FindByIdOrDefaultAsync(shelveId)
+            ?? throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(Shelve), shelveId));
+
+            return shelve;
+        }
+        public async Task<Shelve> GetByTitleAsync(string title)
+        {
+            var shelve = await FindByTitleOrDefaultAsync(title)
+            ?? throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(Shelve), title));
+
+            return shelve;
+        }
         public async Task<Shelve> GetActiveByIdAsync(int shelveId)
         {
             var shelve = await GetByIdAsync(shelveId);
@@ -82,27 +92,11 @@
 
             return shelve;
         }
-
-        public async Task<Shelve> GetByIdAsync(int shelveId)
+        public async Task<ICollection<Shelve>> GetAllActiveAsync()
         {
-            var shelve = await FindByIdOrDefaultAsync(shelveId)
-            ?? throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(Shelve), shelveId));
+            var shelves = await GetAllAsync();
 
-            return shelve;
-        }
-
-        public async Task<Shelve> GetByTitleAsync(string title)
-        {
-            var shelve = await FindByTitleOrDefaultAsync(title)
-            ?? throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(Shelve), title));
-
-            return shelve;
-        }
-        public async Task<ICollection<Shelve>> GetAllAsync()
-        {
-            var shelves = await this.dbContext.Shelves.Where(s => !s.Deleted).ToListAsync();
-
-            return shelves;
+            return shelves.ToList();
         }
 
         public async Task<bool> ContainsActiveByTitleAsync(string title, ICollection<Shelve> shelves)
@@ -119,24 +113,6 @@
                 await Task.Run(() => shelve.Title = title);
             }
 
-        }
-        private static async Task SetCreatorAsync(Shelve shelve, int creatorId)
-        {
-            await Task.Run(() => shelve.CreatorId = creatorId);
-        }
-        private async Task SaveModificationAsync(Shelve shelve, int modifierId)
-        {
-            shelve.LastModifierId = modifierId;
-            shelve.LastModifiedOn = DateTime.UtcNow;
-
-            await this.dbContext.SaveChangesAsync();
-        }
-
-        private async Task<Shelve> FindByIdOrDefaultAsync(int id)
-        {
-            var shelve = await this.dbContext.Shelves.FirstOrDefaultAsync(s => s.Id == id);
-
-            return shelve;
         }
         private async Task<Shelve> FindByTitleOrDefaultAsync(string title)
         {
