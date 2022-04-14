a5cc4e2a43cf31872b94091edabdc36fbf7a9fd4
Milen Dinev <dinev.milen@gmail.com>
2022-04-14 21:49:11 +0300

    Adding DeletedQuoteModel mapping profile

--- Boovey.Services/AutoMapperProfiles/Quote/QuoteMappingProfile.cs
+++ Boovey.Services/AutoMapperProfiles/Quote/QuoteMappingProfile.cs
@@ -14,8 +14,9 @@
             this.CreateMap<CreateQuoteModel, Quote>()
                 .ForMember(e => e.CreatedOn, m => m.MapFrom(d => DateTime.Now))
                 .ForMember(e => e.LastModifiedOn, m => m.MapFrom(d => DateTime.Now));
-            this.CreateMap<Quote, AddedQuoteModel>();
+            this.CreateMap<Quote, CreatedQuoteModel>();
             this.CreateMap<Quote, EditedQuoteModel>();
+            this.CreateMap<Quote, DeletedQuoteModel>();
             this.CreateMap<Quote, AddedFavoriteQuoteModel>()
                 .ForMember(m => m.QuoteId, e => e.MapFrom(q => q.Id))
                 .ForMember(m => m.UserId, e => e.MapFrom(q => q.FavoriteByUsers.Select(u => u.Id).LastOrDefault()));
