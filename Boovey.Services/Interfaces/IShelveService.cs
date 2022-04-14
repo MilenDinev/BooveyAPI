af2d10715a732615472a01619bc7e361a3d23e9f
Milen Dinev <dinev.milen@gmail.com>
2022-04-14 20:21:21 +0300

    Replacing id with shelve as argument in Add and Remove favorite, methods

--- Boovey.Services/Interfaces/IShelveService.cs
+++ Boovey.Services/Interfaces/IShelveService.cs
@@ -12,8 +12,8 @@
         Task EditAsync(Shelve shelve, EditShelveModel shelveModel, int modifierId);
         Task DeleteAsync(Shelve shelve, int modifierId);
 
-        Task<AddedFavoriteShelveModel> AddFavoriteAsync(int Id, User currentUser);
-        Task<RemovedFavoriteShelveModel> RemoveFavoriteAsync(int Id, User currentUser);
+        Task<AddedFavoriteShelveModel> AddFavoriteAsync(Shelve shelve, User currentUser);
+        Task<RemovedFavoriteShelveModel> RemoveFavoriteAsync(Shelve shelve, User currentUser);
 
         Task<bool> ContainsActiveByTitleAsync(string title, ICollection<Shelve> shelves);
         Task<Shelve> GetActiveByIdAsync(int Id);
