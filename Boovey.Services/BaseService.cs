8232cee55493ef244c3bd32b50d396a8abfbd339
Milen Dinev <dinev.milen@gmail.com>
2022-04-14 20:08:18 +0300

    Refactoring  : methods position changed

--- Boovey.Services/BaseService.cs
+++ Boovey.Services/BaseService.cs
@@ -16,30 +16,16 @@
             this.dbContext = dbContext;
         }
 
-        protected async Task<TEntity> AddEntityAsync(TEntity entity, int creatorId)
+        protected async Task AddEntityAsync(TEntity entity, int creatorId)
         {
             await SetCreatorAsync(entity, creatorId);
             await this.dbContext.AddAsync(entity);
             await SaveModificationAsync(entity, creatorId);
-
-            return entity;
         }
         protected async Task DeleteEntityAsync(TEntity entity, int modifierId)
         {
             entity.Deleted = true;
             await SaveModificationAsync(entity, modifierId);
-        }
-        protected async Task SetCreatorAsync(TEntity entity, int creatorId)
-        {
-            entity.CreatorId = creatorId;
-            await SaveModificationAsync(entity, creatorId);
-        }
-        protected async Task SaveModificationAsync(TEntity entity, int modifierId)
-        {
-            entity.LastModifierId = modifierId;
-            entity.LastModifiedOn = DateTime.UtcNow;
-
-            await this.dbContext.SaveChangesAsync();
         }
         protected  async Task<IEnumerable<TEntity>> GetAllAsync()
         {
@@ -52,6 +38,18 @@
             var entity = await this.dbContext.Set<TEntity>().FirstOrDefaultAsync(b => b.Id == id);
 
             return entity;
+        }
+        protected async Task SaveModificationAsync(TEntity entity, int modifierId)
+        {
+            entity.LastModifierId = modifierId;
+            entity.LastModifiedOn = DateTime.UtcNow;
+
+            await this.dbContext.SaveChangesAsync();
+        }
+        private async Task SetCreatorAsync(TEntity entity, int creatorId)
+        {
+            entity.CreatorId = creatorId;
+            await SaveModificationAsync(entity, creatorId);
         }
     }
 }
