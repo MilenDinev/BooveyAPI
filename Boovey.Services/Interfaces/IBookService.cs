9f2ae573c489d00d5bdd238761d31d7454db996d
Milen Dinev <dinev.milen@gmail.com>
2022-04-27 23:52:19 +0300

    AddFavorite and RemoveFavorite methods changed to void and accepting Book as first argument instead of int. Renaming arguments variables in CreateAsync, EditAsync, GetByIdAsync and GetActiveById methods

--- Boovey.Services/Interfaces/IBookService.cs
+++ Boovey.Services/Interfaces/IBookService.cs
@@ -4,17 +4,16 @@
     using System.Threading.Tasks;
     using Data.Entities;
     using Models.Requests.BookModels;
-    using Models.Responses.BookModels;
 
     public interface IBookService
     {
-        Task<Book> CreateAsync(CreateBookModel bookModel, int currentUserId);
-        Task EditAsync(Book book, EditBookModel bookModel, int currentUserId);
+        Task<Book> CreateAsync(CreateBookModel bookModel, int creatorId);
+        Task EditAsync(Book book, EditBookModel bookModel, int creatorId);
         Task DeleteAsync(Book book, int modifierId);
-        Task<AddedFavoriteBookModel> AddFavorite(int bookId, User currentUser);
-        Task<RemovedFavoriteBookModel> RemoveFavorite(int bookId, User currentUser);
-        Task<Book> GetByIdAsync(int bookId);
-        Task<Book> GetActiveByIdAsync(int bookId);
+        Task AddFavorite(Book book, User user);
+        Task RemoveFavorite(Book book, User user);
+        Task<Book> GetByIdAsync(int Id);
+        Task<Book> GetActiveByIdAsync(int Id);
         Task<ICollection<Book>> GetAllActiveAsync();
         Task<bool> ContainsActiveByTitleAsync(string title, ICollection<Book> books);
         Task<Book> AssignAuthorAsync(Book book, int authorId, int modifierId);
