b0cdd36e9e5bf7f4526c08c6dd4bdd5de06c5d68
Milen Dinev <dinev.milen@gmail.com>
2022-04-29 01:59:17 +0300

    Initial creation of IBook interface  which will improve abstraction for Book class .

---
+++ Boovey.Data/Entities/Interfaces/IBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boovey.Data.Entities.Interfaces
+{
+    public interface IBook : IEntity
+    {
+        public string CoverUrl { get; set; }
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+        public string Description { get; set; }
+        public int? PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
+        public ICollection<Quote> Quotes { get; set; }
+        public ICollection<Shelve> Shelves { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+        public ICollection<Author> Authors { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<User> FavoriteByUsers { get; set; }
+    }
+}
