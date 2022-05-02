bafb894a1273a61c281edd003e3deb5ece53a40f
Milen Dinev <dinev.milen@gmail.com>
2022-05-02 18:58:40 +0300

    Handler renamed to Service

---
+++ Boovey.Services/Handlers/AssigningService.cs
@@ -0,0 +1,58 @@
+﻿namespace Boovey.Services.Handlers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Castle.DynamicProxy;
+    using Interfaces.IHandlers;
+    using Data.Entities;
+    using Data.Entities.Interfaces;
+
+    public class AssigningService<TEntity> : IAssigningService<TEntity>
+        where TEntity : class, IBook
+    {
+        private const int BUFFER_LENGHT = 5;
+
+        public async Task AssignAsync(TEntity entity, IAssignable assignee)
+        {
+            var assigneeProxyType = (assignee as IProxyTargetAccessor).DynProxyGetTarget().GetType().Name;
+            var assigneeType = await Task.Run(() => assigneeProxyType.Remove(assigneeProxyType.Length - BUFFER_LENGHT));
+
+            if (assigneeType == "Author")
+            {
+                entity.Authors.Add(assignee as Author);
+            }
+            else if (assigneeType == "Genre")
+            {
+                entity.Genres.Add(assignee as Genre);
+            }
+            else if (assigneeType == "Publisher")
+            {
+                entity.Publisher = assignee as Publisher;
+            }
+        }
+
+        public async Task<bool> IsAlreadyAssigned(TEntity entity, IAssignable assignee)
+        {
+            var isAlreadyAssigned = false;
+            var assigneeProxyType = (assignee as IProxyTargetAccessor).DynProxyGetTarget().GetType().Name;
+            var assigneeType = await Task.Run(() => assigneeProxyType.Remove(assigneeProxyType.Length - BUFFER_LENGHT));
+
+            if (assigneeType == "Author")
+            {
+                assignee = assignee as Author;
+                isAlreadyAssigned = entity.Authors.Any(a => a.Id == assignee.Id);
+            }
+            else if (assigneeType == "Genre")
+            {
+                assignee = assignee as Genre;
+                isAlreadyAssigned = entity.Genres.Any(g => g.Id == assignee.Id);
+            }
+            else if (assigneeType == "Publisher")
+            {
+                assignee = assignee as Publisher;
+                isAlreadyAssigned = entity.PublisherId == assignee.Id;
+            }
+            return await Task.Run(() => isAlreadyAssigned);
+        }
+    }
+}
---
+++ Boovey.Services/Handlers/ContextAccessorService.cs
@@ -0,0 +1,66 @@
+﻿namespace Boovey.Services.Handlers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Microsoft.EntityFrameworkCore;
+    using Constants;
+    using Exceptions;
+    using Interfaces.IHandlers;
+    using Data;
+    using Data.Entities.Interfaces;
+
+    public class ContextAccessorService<TEntity> : IContextAccessorServices<TEntity>
+        where TEntity : class, IAccessible
+    {
+        private readonly BooveyDbContext dbContext;
+
+        public ContextAccessorService(BooveyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            var entity = await FindByIdOrDefaultAsync(id)
+            ?? throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(TEntity).GetType().Name, id));
+
+            return entity;
+        }
+        public async Task<TEntity> GetActiveByIdAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity.Deleted)
+                throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityHasBeenDeleted, nameof(TEntity).GetType().Name));
+
+            return entity;
+        }
+        public async Task<ICollection<TEntity>> GetAllAsync()
+        {
+            var entities = await this.dbContext.Set<TEntity>().ToArrayAsync();
+
+            return entities;
+        }
+        public async Task<ICollection<TEntity>> GetAllActiveAsync()
+        {
+            var entities = await GetAllAsync();
+
+            return entities.Where(s => !s.Deleted).ToList();
+        }
+        public async Task<TEntity> FindByIdOrDefaultAsync(int id)
+        {
+            var entity = await this.dbContext.Set<TEntity>().FirstOrDefaultAsync(e => e.Id == id);
+            return entity;
+        }
+
+        //public async Task<Book> GetByTitleAsync(string title)
+        //{
+        //    var book = await FindByTitleOrDefaultAsync(title)
+        //    ?? throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(Book), title));
+
+        //    return book;
+        //}
+
+    }
+}
+
