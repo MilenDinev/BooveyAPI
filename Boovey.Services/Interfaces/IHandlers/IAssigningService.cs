1de1530b9b87ffc6b484b353e17ec9ec9576bb0b
Milen Dinev <dinev.milen@gmail.com>
2022-05-02 20:04:35 +0300

    Initial Creation

---
+++ Boovey.Services/Interfaces/IHandlers/IAssigningService.cs
@@ -0,0 +1,11 @@
+﻿namespace Boovey.Services.Interfaces.IHandlers
+{
+    using System.Threading.Tasks;
+    using Data.Entities.Interfaces;
+
+    public interface IAssigningService<TEntity>  where TEntity : class, IBook
+    {
+        Task AssignAsync(TEntity entity, IAssignable assignee);
+        Task<bool> IsAlreadyAssigned(TEntity entity, IAssignable assignee);
+    }
+}
---
+++ Boovey.Services/Interfaces/IHandlers/IContextAccessorServices.cs
@@ -0,0 +1,15 @@
+﻿namespace Boovey.Services.Interfaces.IHandlers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Data.Entities.Interfaces;
+
+    public interface IContextAccessorServices<TEntity> where TEntity : class, IAccessible
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<TEntity> GetActiveByIdAsync(int id);
+        Task<ICollection<TEntity>> GetAllAsync();
+        Task<ICollection<TEntity>> GetAllActiveAsync();
+        Task<TEntity> FindByIdOrDefaultAsync(int id);
+    }
+}
