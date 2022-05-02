3ff71137ba3040b553e36b660b76ba8eb342d477
Milen Dinev <dinev.milen@gmail.com>
2022-05-03 01:58:21 +0300

    using accessor service

--- Boovey.Web/Controllers/QuotesController.cs
+++ Boovey.Web/Controllers/QuotesController.cs
@@ -1,73 +1,77 @@
-﻿//namespace Boovey.Web.Controllers
-//{
-//    using System.Threading.Tasks;
-//    using AutoMapper;
-//    using Base;
-//    using Microsoft.AspNetCore.Mvc;
-//    using Models.Requests.QuoteModels;
-//    using Models.Responses.QuoteModels;
-//    using Services.Interfaces;
+﻿namespace Boovey.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using AutoMapper;
+    using Base;
+    using Services.Interfaces;
+    using Services.Interfaces.IHandlers;
+    using Data.Entities;
+    using Models.Requests.QuoteModels;
+    using Models.Responses.QuoteModels;
 
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class QuotesController : BooveyBaseController
-//    {
-//        private readonly IQuoteService quoteService;
-//        private readonly IMapper mapper;
-//        public QuotesController(IUserService userService, IQuoteService quoteService, IMapper mapper) : base(userService)
-//        {
-//            this.quoteService = quoteService;
-//            this.mapper = mapper;
-//        }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : BooveyBaseController
+    {
+        private readonly IQuoteService quoteService;
+        private readonly IContextAccessorServices<Quote> quotesAccessorService;
+        private readonly IMapper mapper;
+        public QuotesController(IQuoteService quoteService, IContextAccessorServices<Quote> quotesAccessorService, IMapper mapper, IUserService userService) : base(userService)
+        {
+            this.quoteService = quoteService;
+            this.quotesAccessorService = quotesAccessorService;
+            this.mapper = mapper;
+        }
 
-//        [HttpPost("Create/")]
-//        public async Task<ActionResult> Add(CreateQuoteModel quoteInput)
-//        {
-//            await AssignCurrentUserAsync();
-//            var createdQuote = await this.quoteService.CreateAsync(quoteInput, CurrentUser.Id);
-//            return CreatedAtAction(nameof(Add), "Quotes", new { id = createdQuote.Id }, createdQuote);
-//        }
+        [HttpPost("Create/")]
+        public async Task<ActionResult> Add(CreateQuoteModel quoteInput)
+        {
+            await AssignCurrentUserAsync();
+            var createdQuote = await this.quoteService.CreateAsync(quoteInput, CurrentUser.Id);
+            return CreatedAtAction(nameof(Add), "Quotes", new { id = createdQuote.Id }, createdQuote);
+        }
 
-//        [HttpPut("Edit/Quote/{quoteId}")]
-//        public async Task<ActionResult<EditedQuoteModel>> Edit(EditQuoteModel quoteInput, int quoteId)
-//        {
-//            await AssignCurrentUserAsync();
+        [HttpPut("Edit/Quote/{quoteId}")]
+        public async Task<ActionResult<EditedQuoteModel>> Edit(EditQuoteModel quoteInput, int quoteId)
+        {
+            await AssignCurrentUserAsync();
 
-//            var quote = await this.quoteService.GetActiveByIdAsync(quoteId);
-//            await this.quoteService.EditAsync(quote, quoteInput, CurrentUser.Id);
+            var quote = await this.quotesAccessorService.GetActiveByIdAsync(quoteId);
+            await this.quoteService.EditAsync(quote, quoteInput, CurrentUser.Id);
 
-//            return mapper.Map<EditedQuoteModel>(quote); ;
-//        }
+            return mapper.Map<EditedQuoteModel>(quote); ;
+        }
 
-//        [HttpPut("Favorites/Add/Quote/{quoteId}")]
-//        public async Task<AddedFavoriteQuoteModel> AddFavorite(int quoteId)
-//        {
-//            await AssignCurrentUserAsync();
+        [HttpPut("Favorites/Add/Quote/{quoteId}")]
+        public async Task<AddedFavoriteQuoteModel> AddFavorite(int quoteId)
+        {
+            await AssignCurrentUserAsync();
 
-//            var quote = await this.quoteService.GetActiveByIdAsync(quoteId);
-//            var addedFavoriteQuote = await this.quoteService.AddFavoriteAsync(quote, CurrentUser);
+            var quote = await this.quotesAccessorService.GetActiveByIdAsync(quoteId);
+            var addedFavoriteQuote = await this.quoteService.AddFavoriteAsync(quote, CurrentUser);
 
-//            return addedFavoriteQuote;
-//        }
+            return addedFavoriteQuote;
+        }
 
-//        [HttpPut("Favorites/Remove/Quote/{quoteId}")]
-//        public async Task<RemovedFavoriteQuoteModel> RemoveFavorite(int quoteId)
-//        {
-//            await AssignCurrentUserAsync();
+        [HttpPut("Favorites/Remove/Quote/{quoteId}")]
+        public async Task<RemovedFavoriteQuoteModel> RemoveFavorite(int quoteId)
+        {
+            await AssignCurrentUserAsync();
 
-//            var quote = await this.quoteService.GetActiveByIdAsync(quoteId);
-//            var removedFavoriteQuote = await this.quoteService.RemoveFavoriteAsync(quote, CurrentUser);
+            var quote = await this.quotesAccessorService.GetActiveByIdAsync(quoteId);
+            var removedFavoriteQuote = await this.quoteService.RemoveFavoriteAsync(quote, CurrentUser);
 
-//            return removedFavoriteQuote;
-//        }
+            return removedFavoriteQuote;
+        }
 
-//        [HttpDelete("Delete/Quote/{quoteId}")]
-//        public async Task<DeletedQuoteModel> Delete(int quoteId)
-//        {
-//            await AssignCurrentUserAsync();
-//            var quote = await this.quoteService.GetActiveByIdAsync(quoteId);
-//            await this.quoteService.DeleteAsync(quote, CurrentUser.Id);
-//            return mapper.Map<DeletedQuoteModel>(quote);
-//        }
-//    }
-//}
+        [HttpDelete("Delete/Quote/{quoteId}")]
+        public async Task<DeletedQuoteModel> Delete(int quoteId)
+        {
+            await AssignCurrentUserAsync();
+            var quote = await this.quotesAccessorService.GetActiveByIdAsync(quoteId);
+            await this.quoteService.DeleteAsync(quote, CurrentUser.Id);
+            return mapper.Map<DeletedQuoteModel>(quote);
+        }
+    }
+}
--- Boovey.Web/Controllers/ReviewsController.cs
+++ Boovey.Web/Controllers/ReviewsController.cs
@@ -1,49 +1,53 @@
-﻿//namespace Boovey.Web.Controllers
-//{
-//    using System.Threading.Tasks;
-//    using Microsoft.AspNetCore.Mvc;
-//    using AutoMapper;
-//    using Base;
-//    using Services.Interfaces;
-//    using Models.Requests.ReviewModels;
-//    using Models.Responses.ReviewModels;
+﻿namespace Boovey.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using AutoMapper;
+    using Base;
+    using Services.Interfaces;
+    using Services.Interfaces.IHandlers;
+    using Data.Entities;
+    using Models.Requests.ReviewModels;
+    using Models.Responses.ReviewModels;
 
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class ReviewsController : BooveyBaseController
-//    {
-//        private readonly IReviewService reviewService;
-//        private readonly IMapper mapper;
-//        public ReviewsController(IUserService userService, IReviewService reviewService, IMapper mapper) : base(userService)
-//        {
-//            this.reviewService = reviewService;
-//            this.mapper = mapper;
-//        }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : BooveyBaseController
+    {
+        private readonly IReviewService reviewService;
+        private readonly IContextAccessorServices<Review> reviewsAccessorService;
+        private readonly IMapper mapper;
+        public ReviewsController(IReviewService reviewService, IContextAccessorServices<Review> reviewsAccessorService, IMapper mapper, IUserService userService) : base(userService)
+        {
+            this.reviewService = reviewService;
+            this.reviewsAccessorService = reviewsAccessorService;
+            this.mapper = mapper;
+        }
 
-//        [HttpPost("Create/")]
-//        public async Task<ActionResult> Add(CreateReviewModel reviewInput)
-//        {
-//            await AssignCurrentUserAsync();
-//            var addedReview = await this.reviewService.CreateAsync(reviewInput, CurrentUser.Id);
-//            return CreatedAtAction(nameof(Add), "Reviews", new { id = addedReview.Id }, addedReview);
-//        }
+        [HttpPost("Create/")]
+        public async Task<ActionResult> Add(CreateReviewModel reviewInput)
+        {
+            await AssignCurrentUserAsync();
+            var addedReview = await this.reviewService.CreateAsync(reviewInput, CurrentUser.Id);
+            return CreatedAtAction(nameof(Add), "Reviews", new { id = addedReview.Id }, addedReview);
+        }
 
-//        [HttpPut("Edit/Review/{reviewId}")]
-//        public async Task<ActionResult<EditedReviewModel>> Edit(EditReviewModel reviewInput, int reviewId)
-//        {
-//            await AssignCurrentUserAsync();
-//            var review = await this.reviewService.GetByIdAsync(reviewId);
-//            await this.reviewService.EditAsync(review, reviewInput, CurrentUser.Id);
-//            return mapper.Map<EditedReviewModel>(review);
-//        }
+        [HttpPut("Edit/Review/{reviewId}")]
+        public async Task<ActionResult<EditedReviewModel>> Edit(EditReviewModel reviewInput, int reviewId)
+        {
+            await AssignCurrentUserAsync();
+            var review = await this.reviewsAccessorService.GetActiveByIdAsync(reviewId);
+            await this.reviewService.EditAsync(review, reviewInput, CurrentUser.Id);
+            return mapper.Map<EditedReviewModel>(review);
+        }
 
-//        [HttpDelete("Delete/Review/{reviewId}")]
-//        public async Task<DeletedReviewModel> Delete(int reviewId)
-//        {
-//            await AssignCurrentUserAsync();
-//            var review = await this.reviewService.GetActiveByIdAsync(reviewId);
-//            await this.reviewService.DeleteAsync(review, CurrentUser.Id);
-//            return mapper.Map<DeletedReviewModel>(review);
-//        }
-//    }
-//}
+        [HttpDelete("Delete/Review/{reviewId}")]
+        public async Task<DeletedReviewModel> Delete(int reviewId)
+        {
+            await AssignCurrentUserAsync();
+            var review = await this.reviewsAccessorService.GetActiveByIdAsync(reviewId);
+            await this.reviewService.DeleteAsync(review, CurrentUser.Id);
+            return mapper.Map<DeletedReviewModel>(review);
+        }
+    }
+}
