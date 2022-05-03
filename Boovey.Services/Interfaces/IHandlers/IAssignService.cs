a81b7d868705a73535a74e2cabdbc2bf6c7835ba
Milen Dinev <dinev.milen@gmail.com>
2022-05-04 02:04:21 +0300

    Renaming IAssigningService to IAssignService, adding assigneeType argument in Assigning methods

---
+++ Boovey.Services/Interfaces/IHandlers/IAssignService.cs
@@ -0,0 +1,11 @@
+﻿namespace Boovey.Services.Interfaces.IHandlers
+{
+    using System.Threading.Tasks;
+    using Data.Entities.Interfaces;
+
+    public interface IAssignService<TEntity>  where TEntity : class, IBook
+    {
+        Task AssignAsync(TEntity entity, IAssignable assignee, string assigneeType);
+        Task<bool> IsAlreadyAssigned(TEntity entity, IAssignable assignee, string assigneeType);
+    }
+}
