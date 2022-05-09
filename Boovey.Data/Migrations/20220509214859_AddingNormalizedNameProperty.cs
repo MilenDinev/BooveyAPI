0c298c08cac64f7941c0074ad5f20d43308dccd3
Milen Dinev <dinev.milen@gmail.com>
2022-05-10 00:49:34 +0300

    NormalizedName property migration

---
+++ Boovey.Data/Migrations/20220509214859_AddingNormalizedNameProperty.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Boovey.Data.Migrations
+{
+    public partial class AddingNormalizedNameProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Shelves",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "RequestType",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "RequestStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Requests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Quotes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Publishers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Countries",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatorId",
+                table: "Countries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedOn",
+                table: "Countries",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastModifierId",
+                table: "Countries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Shelves");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "RequestType");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "RequestStatus");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Quotes");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Publishers");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Genres");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedOn",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierId",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Authors");
+        }
+    }
+}
