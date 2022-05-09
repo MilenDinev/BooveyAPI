a2a06543307cfa5eaba9a834d34233a2c361b469
Milen Dinev <dinev.milen@gmail.com>
2022-05-10 02:10:06 +0300

    Declared ContainsActiveByStringAsync, GetActiveByStringAsync, GetByStringAsync, FindByStringOrDefaultAsync methods

--- Boovey.Services/Interfaces/IHandlers/ISearchService.cs
+++ Boovey.Services/Interfaces/IHandlers/ISearchService.cs
@@ -7,9 +7,13 @@
     public interface ISearchService<TEntity> where TEntity : class, ISearchable
     {
         Task<TEntity> GetByIdAsync(int id, string type);
+        Task<TEntity> GetByStringAsync(string stringValue, string type);
         Task<TEntity> GetActiveByIdAsync(int id, string type);
+        Task<TEntity> GetActiveByStringAsync(string stringValue, string type);
         Task<ICollection<TEntity>> GetAllAsync();
         Task<ICollection<TEntity>> GetAllActiveAsync();
         Task<TEntity> FindByIdOrDefaultAsync(int id);
+        Task<TEntity> FindByStringOrDefaultAsync(string stringValue);
+        Task<bool> ContainsActiveByStringAsync(string stringValue);
     }
 }
