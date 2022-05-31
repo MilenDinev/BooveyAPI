1013daf99ca4445b36edf391ce4e01b78a1945e0
Milen Dinev <dinev.milen@gmail.com>
2022-05-31 03:11:05 +0300

    Deleting due optimization

--- Boovey.Data/Entities/Interfaces/ISearchable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Boovey.Data.Entities.Interfaces
-{
-    public interface ISearchable
-    {
-        public int Id { get; }
-        public string NormalizedName { get; }
-        public bool Deleted { get; }
-    }
-}
