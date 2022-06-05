ba89341d90c7a7fa92db47c9586d37cfde106c79
Milen Dinev <dinev.milen@gmail.com>
2022-06-05 23:55:46 +0300

    Refactoring Searching service

--- Boovey.Services/Handlers/SearchService.cs
+++ Boovey.Services/Handlers/SearchService.cs
@@ -8,10 +8,9 @@
     using Exceptions;
     using Interfaces.IHandlers;
     using Data;
-    using Data.Entities.Interfaces;
+    using Data.Entities.Interfaces.IEntities;
 
-    public class SearchService<TEntity> : ISearchService<TEntity>
-        where TEntity : class, ISearchable
+    public class SearchService : ISearchService
     {
         private readonly BooveyDbContext dbContext;
 
@@ -20,83 +19,85 @@
             this.dbContext = dbContext;
         }
 
-        public async Task<TEntity> FindByIdOrDefaultAsync(int id)
+        public async Task<T> FindByIdOrDefaultAsync<T>(int id) where T : class, IEntity
         {
-            var entity = await this.dbContext.Set<TEntity>().FirstOrDefaultAsync(e => e.Id == id);
+            var entity = await this.dbContext.Set<T>().FirstOrDefaultAsync(e => e.Id == id);
             return entity;
         }
-        public async Task<TEntity> FindByStringOrDefaultAsync(string stringValue)
+        public async Task<T> FindByStringOrDefaultAsync<T>(string stringValue) where T : class, IEntity
         {
-            var entity = await this.dbContext.Set<TEntity>().FirstOrDefaultAsync(e => e.NormalizedName == stringValue.ToUpper());
+            var entity = await this.dbContext.Set<T>().FirstOrDefaultAsync(e => e.NormalizedName == stringValue.ToUpper());
             return entity;
         }
-        public async Task<TEntity> GetByIdAsync(int id, string type)
+        public async Task<T> GetByIdAsync<T>(int id) where T : class, IEntity
         {
-            var entity = await FindByIdOrDefaultAsync(id);
-            await ExistenceChecker(entity, id.ToString(), type);
+            var entity = await FindByIdOrDefaultAsync<T>(id);
+            await ExistenceChecker(entity, id.ToString());
 
             return entity;
         }
-        public async Task<TEntity> GetByStringAsync(string stringValue, string type)
+        public async Task<T> GetByStringAsync<T>(string stringValue) where T : class, IEntity
         {
-            var entity = await FindByStringOrDefaultAsync(stringValue);
-            await ExistenceChecker(entity, stringValue, type);
+            var entity = await FindByStringOrDefaultAsync<T>(stringValue);
+            await ExistenceChecker(entity, stringValue);
 
             return entity;
         }
-        public async Task<TEntity> GetActiveByIdAsync(int id, string type)
+        public async Task<T> GetActiveByIdAsync<T>(int id) where T : class, IEntity
         {
-            var entity = await GetByIdAsync(id, type);
-            await DeletedChecker(entity.Deleted, id.ToString(), type);
+            var entity = await GetByIdAsync<T>(id);
+            await DeletedChecker(entity.Deleted, id.ToString());
 
             return entity;
         }
-        public async Task<TEntity> GetActiveByStringAsync(string stringValue, string type)
+        public async Task<T> GetActiveByStringAsync<T>(string stringValue) where T : class, IEntity
         {
-            var entity = await GetByStringAsync(stringValue, type);
-            await DeletedChecker(entity.Deleted, stringValue, type);
+            var entity = await GetByStringAsync<T>(stringValue);
+            await DeletedChecker(entity.Deleted, stringValue);
 
             return entity;
         }
-        public async Task<ICollection<TEntity>> GetAllAsync()
+
+        public async Task<ICollection<T>> GetAllAsync<T>() where T : class, IEntity
         {
-            var entities = await this.dbContext.Set<TEntity>().ToArrayAsync();
+            var entities = await this.dbContext.Set<T>().ToArrayAsync();
 
             return entities;
         }
-        public async Task<ICollection<TEntity>> GetAllActiveAsync()
+        public async Task<ICollection<T>> GetAllActiveAsync<T>() where T : class, IEntity
         {
-            var entities = await GetAllAsync();
+            var entities = await GetAllAsync<T>();
 
             return entities.Where(s => !s.Deleted).ToList();
         }
 
 
-        public async Task<bool> ContainsActiveByStringAsync(string stringValue)
+        public async Task<bool> ContainsActiveByStringAsync<T>(string stringValue) where T : class, IEntity
         {
-            var entities = await this.dbContext.Set<TEntity>().ToListAsync();
-            return await ContainsActiveByStringAsync(stringValue, entities);
+            var entities = await this.dbContext.Set<T>().ToListAsync();
+            return await ContainsActiveByStringAsync<T>(stringValue, entities);
         }
 
-        public async Task<bool> ContainsActiveByStringAsync(string stringValue, ICollection<TEntity> collection)
+        public async Task<bool> ContainsActiveByStringAsync<T>(string stringValue, ICollection<T> collection) where T : class, IEntity
         {
             var contains = collection.Any(e => e.NormalizedName == stringValue.ToUpper() && !e.Deleted);
 
             return await Task.FromResult(contains);
         }
 
-        private async Task ExistenceChecker(TEntity entity, string searchFlag, string type)
+        private async Task ExistenceChecker(IEntity entity, string searchFlag)
         {
             if (await Task.Run(() => entity == null))
-                throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, type, searchFlag));
+                throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, "test", searchFlag));
         }
-        private async Task DeletedChecker(bool deleted, string searchFlag, string type)
+        private async Task DeletedChecker(bool deleted, string searchFlag)
         {
             if (await Task.Run(() => deleted))
-                throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityHasBeenDeleted, type, searchFlag));
+                throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityHasBeenDeleted, "test", searchFlag));
 
         }
 
+       
     }
 }
 
