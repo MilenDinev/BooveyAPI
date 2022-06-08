77f297885c64384f482732519f7c7fdecabb2c49
Milen Dinev <dinev.milen@gmail.com>
2022-06-09 00:02:02 +0300

    Refactoring : removing unused methods

--- Boovey.Services/Handlers/SearchService.cs
+++ Boovey.Services/Handlers/SearchService.cs
@@ -4,8 +4,6 @@
     using System.Threading.Tasks;
     using System.Collections.Generic;
     using Microsoft.EntityFrameworkCore;
-    using Constants;
-    using Exceptions;
     using Interfaces.IHandlers;
     using Data;
     using Data.Entities.Interfaces.IEntities;
@@ -29,34 +27,6 @@
             var entity = await this.dbContext.Set<T>().FirstOrDefaultAsync(e => e.NormalizedName == stringValue.ToUpper());
             return entity;
         }
-        public async Task<T> GetByIdAsync<T>(int id) where T : class, IEntity
-        {
-            var entity = await FindByIdOrDefaultAsync<T>(id);
-            await ExistenceChecker(entity, id.ToString());
-
-            return entity;
-        }
-        public async Task<T> GetByStringAsync<T>(string stringValue) where T : class, IEntity
-        {
-            var entity = await FindByStringOrDefaultAsync<T>(stringValue);
-            await ExistenceChecker(entity, stringValue);
-
-            return entity;
-        }
-        public async Task<T> GetActiveByIdAsync<T>(int id) where T : class, IEntity
-        {
-            var entity = await GetByIdAsync<T>(id);
-            await DeletedChecker(entity.Deleted, id.ToString());
-
-            return entity;
-        }
-        public async Task<T> GetActiveByStringAsync<T>(string stringValue) where T : class, IEntity
-        {
-            var entity = await GetByStringAsync<T>(stringValue);
-            await DeletedChecker(entity.Deleted, stringValue);
-
-            return entity;
-        }
 
         public async Task<ICollection<T>> GetAllAsync<T>() where T : class, IEntity
         {
@@ -70,33 +40,6 @@
 
             return entities.Where(s => !s.Deleted).ToList();
         }
-
-
-        public async Task<bool> ContainsActiveByStringAsync<T>(string stringValue) where T : class, IEntity
-        {
-            var entities = await this.dbContext.Set<T>().ToListAsync();
-            return await ContainsActiveByStringAsync<T>(stringValue, entities);
-        }
-
-        public async Task<bool> ContainsActiveByStringAsync<T>(string stringValue, ICollection<T> collection) where T : class, IEntity
-        {
-            var contains = collection.Any(e => e.NormalizedName == stringValue.ToUpper() && !e.Deleted);
-
-            return await Task.FromResult(contains);
-        }
-
-        private async Task ExistenceChecker(IEntity entity, string searchFlag)
-        {
-            if (await Task.Run(() => entity == null))
-                throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, "test", searchFlag));
-        }
-        private async Task DeletedChecker(bool deleted, string searchFlag)
-        {
-            if (await Task.Run(() => deleted))
-                throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityHasBeenDeleted, "test", searchFlag));
-
-        }
-
        
     }
 }
