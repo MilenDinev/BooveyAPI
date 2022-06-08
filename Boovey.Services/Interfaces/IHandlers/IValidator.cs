6c5a7fbf27fae2457a789cd10c9b3c98248da7ec
Milen Dinev <dinev.milen@gmail.com>
2022-06-08 21:58:06 +0300

    Initial creation of IValidator Interface

---
+++ Boovey.Services/Interfaces/IHandlers/IValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Boovey.Services.Interfaces.IHandlers
+{
+    using System.Threading.Tasks;
+    using Data.Entities.Interfaces.IEntities;
+
+    public interface IValidator
+    {
+        Task<bool> ValidateEntityAsync<T>(T entity, string flag) where T : class, IEntity;
+        Task<bool> ValidateUniqueEntityAsync<T>(T entity) where T : class, IEntity;
+    }
+}
