bc8b894dc2a0c482a2121b123ca0acd83d55eb0c
Milen Dinev <dinev.milen@gmail.com>
2022-06-08 22:00:18 +0300

    Initial creation of  Interface IEntityChecker

---
+++ Boovey.Services/Interfaces/IHandlers/IEntityChecker.cs
@@ -0,0 +1,13 @@
+﻿namespace Boovey.Services.Interfaces.IHandlers
+{
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Data.Entities.Interfaces.IEntities;
+
+    public interface IEntityChecker
+    {
+        Task<bool> DuplicationChecker<T>(string searchFlag, ICollection<T> collection) where T : class, IEntity;
+        Task NullableHandler<T>(T entity, string searchFlag) where T : class, IEntity;
+        Task DeletedHandler<T>(T entity) where T : class, IEntity;
+    }
+}
