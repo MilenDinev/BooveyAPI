297724a348aa28ce4de86b3d84d8c1efa3a8c7c4
Milen Dinev <dinev.milen@gmail.com>
2022-06-09 00:01:11 +0300

    Renaming and implementing : Assigning related checks

--- Boovey.Services/Handlers/EntityChecker.cs
+++ Boovey.Services/Handlers/EntityChecker.cs
@@ -7,7 +7,9 @@
     using Exceptions;
     using Data;
     using Interfaces.IHandlers;
+    using Data.Entities;
     using Data.Entities.Interfaces.IEntities;
+    using Data.Entities.Interfaces.IAssignables;
 
     public class EntityChecker : IEntityChecker
     {
@@ -18,7 +20,7 @@
             this.dbContext = dbContext;
         }
 
-        public async Task<bool> DuplicationChecker<T>(string searchFlag, ICollection<T> collection) where T : class, IEntity 
+        public async Task<bool> DuplicationCheck<T>(string searchFlag, ICollection<T> collection) where T : class, IEntity 
         {
             var contains = collection.Any(e => e.NormalizedName == searchFlag.ToUpper());
 
@@ -26,16 +28,36 @@
                 throw new ResourceAlreadyExistsException(string.Format(ErrorMessages.EntityAlreadyContained, nameof(T), searchFlag));
             return await Task.FromResult(contains);
         }
-        public async Task NullableHandler<T>(T entity, string searchFlag) where T : class, IEntity
+        public async Task NullableCheck<T>(T entity, string searchFlag) where T : class, IEntity
         {
             if (await Task.Run(() => entity == null))
                 throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityIdDoesNotExist, nameof(T), searchFlag));
         }
-        public async Task DeletedHandler<T>(T entity) where T : class, IEntity
+        public async Task DeletedCheck<T>(T entity) where T : class, IEntity
         {
             if (await Task.Run(() => entity.Deleted))
                 throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityHasBeenDeleted, nameof(T)));
 
+        }
+
+        public async Task<bool> AuthorAssignedCheck<T>(T entity, Author author) where T : class, IAuthorAssignable
+        {
+            return await Task.Run(() => entity.Authors.Any(a => a.Id == author.Id));
+        }
+
+        public async Task<bool> GenreAssignedCheck<T>(T entity, Genre genre) where T : class, IGenreAssignable
+        {
+            return await Task.Run(() => entity.Genres.Any(g => g.Id == genre.Id));
+        }
+
+        public async Task<bool> PublisherAssignedCheck<T>(T entity, Publisher publisher) where T : class, IPublisherAssignable
+        {
+            return await Task.Run(() => entity.PublisherId == publisher.Id);
+        }
+
+        public async Task<bool> BookAssignedCheck<T>(T entity, Book book) where T : class, IBookAssignable
+        {
+            return await Task.Run(() => entity.Books.Any(b => b.Id == book.Id));
         }
     }
 }
