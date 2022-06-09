d8040c5e72c3b26a26a5a96fb9c9982e84373e65
Milen Dinev <dinev.milen@gmail.com>
2022-06-10 00:15:55 +0300

    changing directory

---
+++ Boovey.Services/Base/BaseService.cs
@@ -0,0 +1,44 @@
+﻿namespace Boovey.Services.Base
+{
+    using System;
+    using System.Threading.Tasks;
+    using Data;
+    using Data.Entities.Interfaces.IEntities;
+
+    public abstract class BaseService<TEntity> where TEntity : class, IEntity
+    {
+        protected readonly BooveyDbContext dbContext;
+
+        protected BaseService(BooveyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        protected async Task CreateEntityAsync(TEntity entity, int creatorId)
+        {
+            await AddEntityAsync(entity, creatorId);
+            await SaveModificationAsync(entity, creatorId);
+        }
+        protected async Task DeleteEntityAsync(TEntity entity, int modifierId)
+        {
+            entity.Deleted = true;
+            await SaveModificationAsync(entity, modifierId);
+        }
+
+
+        public async Task SaveModificationAsync(TEntity entity, int modifierId)
+        {
+            entity.LastModifierId = modifierId;
+            entity.LastModifiedOn = DateTime.UtcNow;
+
+            await dbContext.SaveChangesAsync();
+        }
+        private async Task AddEntityAsync(TEntity entity, int creatorId)
+        {
+
+            entity.CreatorId = creatorId;
+            entity.CreatedOn = DateTime.UtcNow;
+            await dbContext.AddAsync(entity);
+        }
+    }
+}
