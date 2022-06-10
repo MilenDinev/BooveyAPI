5be8de90caa4b6e47380ade668cc070ea7a590cc
Milen Dinev <dinev.milen@gmail.com>
2022-06-11 01:42:30 +0300

    Removing unused methods and adding returning type on methods for consistance

--- Boovey.Services/Interfaces/IHandlers/IEntityChecker.cs
+++ Boovey.Services/Interfaces/IHandlers/IEntityChecker.cs
@@ -1,18 +1,15 @@
 ﻿namespace Boovey.Services.Interfaces.IHandlers
 {
     using System.Threading.Tasks;
-    using System.Collections.Generic;
     using Data.Entities;
     using Data.Entities.Interfaces.IEntities;
     using Data.Entities.Interfaces.IAssignables;
 
     public interface IEntityChecker
     {
-        Task<bool> DuplicationCheck<T>(string searchFlag, ICollection<T> collection) where T : class, IEntity;
-        Task<bool> DuplicationCheck<T>(int searchFlag, ICollection<T> collection) where T : class, IEntity;
 
-        Task NullableCheck<T>(T entity, string searchFlag) where T : class, IEntity;
-        Task DeletedCheck<T>(T entity) where T : class, IEntity;
+        Task<bool> NullableCheck<T>(T entity, string searchFlag) where T : class, IEntity;
+        Task<bool> DeletedCheck<T>(T entity) where T : class, IEntity;
 
         Task<bool> AuthorAssignedCheck<T>(T entity, Author author) where T : class, IAuthorAssignable;
         Task<bool> GenreAssignedCheck<T>(T entity, Genre genre) where T : class, IGenreAssignable;
