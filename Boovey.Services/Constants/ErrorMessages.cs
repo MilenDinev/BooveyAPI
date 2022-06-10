b323b9942fd599407cef024488db6acd3e7d3e19
Milen Dinev <dinev.milen@gmail.com>
2022-06-11 01:39:53 +0300

    Changing error messages

--- Boovey.Services/Constants/ErrorMessages.cs
+++ Boovey.Services/Constants/ErrorMessages.cs
@@ -4,14 +4,14 @@
     {
         public const string InvalidCredentials = "Invalid credentials, please try again!";
         public const string InvalidPublicationDate = "Please provide valid publication date in format 'dd/MM/yyyy'!";
-        public const string EntityDoesNotExist = @"{0} '{1}' does not exist!";
-        public const string EntityIdDoesNotExist = @"{0} with id '{1}' does not exist!";
-        public const string EntityAlreadyExists = @"{0} '{1}' already exists in our system!";
-        public const string EntityAlreadyAssignedId = @"{0} with id '{1}' has already been assigned to {2} with id '{3}'!";
-        public const string EntityAlreadyContained = @"{0} has already exists in current collection!";
-        public const string EntityHasBeenDeleted = @"{0} has already been deleted and cannot be modified!";
-        public const string AlreadyFavoriteId = @"{0} with id '{1}' already exists in favorites!";
-        public const string NotFavoriteId = @"{0} with id '{1}' does not exists in favorites!";
+        public const string EntityDoesNotExist = @"Entity does not exist!";
+        public const string EntityIdDoesNotExist = @"Entity with id '{1}' does not exist!";
+        public const string EntityAlreadyExists = @"Entity already exists in our system!";
+        public const string EntityAlreadyAssignedId = @"Entity with id '{1}' has already been assigned!";
+        public const string EntityAlreadyContained = @"Entity already exists in this collection!";
+        public const string EntityHasBeenDeleted = @"Entity has already been deleted and cannot be modified!";
+        public const string AlreadyFavoriteId = @"Entity with id '{1}' already exists in favorites!";
+        public const string NotFavoriteId = @"Entity with id '{1}' does not exists in favorites!";
         public const string AlreadyFollowing = @"{0} '{1}' has already been followed by you!";
         public const string FollowingItSelf = @"Following yourself is forbidden!";
     }
