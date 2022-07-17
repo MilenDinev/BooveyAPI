741556cc707197caabb4a0b03280349d322b0c94
Milen Dinev <dinev.milen@gmail.com>
2022-07-17 21:39:37 +0300

    Refactoring mapping profiles

--- Boovey.Services/AutoMapperProfiles/User/UserMappingProfile.cs
+++ Boovey.Services/AutoMapperProfiles/User/UserMappingProfile.cs
@@ -3,20 +3,23 @@
     using System.Linq;
     using AutoMapper;
     using Data.Entities;
-    using Models.Requests;
+    using Models.Requests.UserModels;
     using Models.Responses.UserModels;
 
     public class UserMappingProfile : Profile
     {
         public UserMappingProfile()
         {
-            this.CreateMap<RegistrationModel, User>();
-            this.CreateMap<User, RegisteredUserModel>();
-            this.CreateMap<User, UsersListingModel>()
-                .ForMember(m => m.Email, e => e.MapFrom(u => u.Email ?? "none"));
+            this.CreateMap<CreateUserModel, User>()
+            .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.UserName.ToUpper()));
+            this.CreateMap<User, CreatedUserModel>();
+            this.CreateMap<User, EditedUserModel>();
+            this.CreateMap<User, DeletedUserModel>();
             this.CreateMap<User, FollowerModel>()
                 .ForMember(m => m.FollowerId, e => e.MapFrom(u => u.Id))
                 .ForMember(m => m.FollowedId, e => e.MapFrom(u => u.Following.Select(u => u.Id).LastOrDefault()));
+            this.CreateMap<User, UserListingModel>()
+                .ForMember(m => m.Email, e => e.MapFrom(u => u.Email ?? "none"));
         }
     }
 }
