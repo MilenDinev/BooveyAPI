a302a1460b04863cf6eed0fefc4971ba810cd6da
Milen Dinev <dinev.milen@gmail.com>
2022-08-27 17:49:45 +0300

    Initial  creation of IFollowersManager interface with declaration of Follow and Unfollow methods

---
+++ Boovey.Services/Managers/Interfaces/IFollowersManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Boovey.Services.Managers.Interfaces
+{
+    using System.Threading.Tasks;
+    using Data.Entities;
+
+    public interface IFollowersManager
+    {
+        Task Follow(User follower, User followed);
+        Task Unfollow(User follower, User followed);
+    }
+}
