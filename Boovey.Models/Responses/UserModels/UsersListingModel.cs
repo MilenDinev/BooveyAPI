dcacc02a951c95b81da8b439e8ea6ee544bb03cc
Milen Dinev <dinev.milen@gmail.com>
2022-08-28 18:33:01 +0300

    Adding new properties to User Listing Model

--- Boovey.Models/Responses/UserModels/UsersListingModel.cs
+++ Boovey.Models/Responses/UserModels/UsersListingModel.cs
@@ -1,11 +1,28 @@
 ﻿namespace Boovey.Models.Responses.UserModels
 {
+    using System.Collections.Generic;
+
     public class UserListingModel
     {
+        public UserListingModel()
+        {
+            this.FavoriteAuthors = new HashSet<string>();
+            this.FavoriteBooks = new HashSet<string>();
+            this.FavoriteGenres = new HashSet<string>();
+            this.Followers = new HashSet<string>();
+            this.Following = new HashSet<string>();
+        }
+
         public int Id { get; set; }
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public ICollection<string> FavoriteAuthors { get; set; }
+        public ICollection<string> FavoriteBooks { get; set; }
+        public ICollection<string> FavoriteGenres { get; set; }
+        public ICollection<string> Followers { get; set; }
+        public ICollection<string> Following { get; set; }
+
     }
 }
